cf455dc689c526cea757665f53550d348b46bc2d
haxscramper <haxscramper@gmail.com>
2023-02-12 02:06:02 +0400

    add yaml output for node/token groups

--- src/lexbase/YamlIO.hpp
+++ src/lexbase/YamlIO.hpp
@@ -23,3 +23,37 @@ YAML::Node yamlRepr(NodeAdapter<N, K> node) {
     }
     return result;
 }
+
+template <typename N, typename K>
+YAML::Node yamlRepr(CR<NodeGroup<N, K>> group, bool withStrings = true) {
+    YAML::Node out;
+    for (const auto& [id, node] : group.nodes.pairs()) {
+        YAML::Node item;
+        item["kind"] = to_string(node->kind);
+        if (node->isTerminal()) {
+            TokenId<K> tokenId = node->getToken();
+            item["tok_idx"]    = tokenId.getIndex();
+            if (withStrings) {
+                item["str"] = group.strVal(id).toBase();
+            }
+        } else {
+            item["extent"] = node->getExtent();
+        }
+        out.push_back(item);
+    }
+    return out;
+}
+
+
+template <typename K>
+YAML::Node yamlRepr(CR<TokenGroup<K>> group) {
+    YAML::Node out;
+    for (const auto& [id, token] : group.tokens.pairs()) {
+        YAML::Node item;
+        item["idx"]  = id.getIndex();
+        item["kind"] = to_string(token->kind);
+        item["str"]  = token->strVal().toBase();
+        out.push_back(item);
+    }
+    return out;
+}
--- tests/tOrgE2EParse.cpp
+++ tests/tOrgE2EParse.cpp
@@ -474,6 +474,10 @@ TEST_CASE("Simple node conversion") {
         p.treeRepr();
         auto node = yamlRepr(p.a(0));
         std::cout << "yaml node\n" << node << "\nend yaml node\n";
+        std::cout << "flat repr\n"
+                  << yamlRepr(p.nodes) << "\nend flat nodes\n";
+        std::cout << "token repr\n"
+                  << yamlRepr(p.tokens) << "\nend flat tokens\n";
     }
 
     SECTION("Diff compilation") {
