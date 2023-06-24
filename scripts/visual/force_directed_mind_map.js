2e731d9a36ec13cc66fd78ec7b2c97db40d3cc8f
haxscramper <haxscramper@gmail.com>
2023-06-24 14:30:36 +0400

    Reformat JS using clang-format

--- scripts/visual/force_directed_mind_map.js
+++ scripts/visual/force_directed_mind_map.js
@@ -1,135 +1,134 @@
-import { convertMindMapGraph } from "./utils.js";
-
-
+import {convertMindMapGraph} from "./utils.js";
 
 // Define color scale
-const colorScale = d3.scaleSequential()
-  .domain([0, 10])  // input domain: levels range from 0 to 10
-  .interpolator(d3.interpolateCool);  // output range: color from warm to cool
+const colorScale =
+    d3.scaleSequential()
+        .domain([ 0, 10 ]) // input domain: levels range from 0 to 10
+        .interpolator(
+            d3.interpolateCool); // output range: color from warm to cool
 
 // Define radius scale
-const radiusScale = d3.scaleLinear()
-  .domain([0, 10])  // input domain: levels range from 0 to 10
-  .range([20, 2]);  // output range: radius size from 20 to 2
+const radiusScale =
+    d3.scaleLinear()
+        .domain([ 0, 10 ]) // input domain: levels range from 0 to 10
+        .range([ 20, 2 ]); // output range: radius size from 20 to 2
 
+d3.json("/tmp/mindmap_graph.json")
+    .then(
+        function(data) {
+          // Specify the dimensions of the chart.
+          const width = 1400;
+          const height = 1400;
 
+          var [nodes, links] = convertMindMapGraph(data);
 
-d3.json("/tmp/mindmap_graph.json").then(
-  function (data) {
-    // Specify the dimensions of the chart.
-    const width = 1400;
-    const height = 1400;
+          // Create the SVG container.
+          const svg = d3.select("body")
+                          .append("svg")
+                          .attr("height", height)
+                          .attr("width", width);
 
-    var [nodes, links] = convertMindMapGraph(data);
+          // Add a line for each link, and a circle for each node.
+          const link = svg.append("g")
+                           .attr("stroke", "#999")
+                           .attr("stroke-opacity", 0.6)
+                           .selectAll("line")
+                           .data(links)
+                           .join("line")
+                           .attr("stroke-width", d => Math.sqrt(d.value));
 
-    // Create the SVG container.
-    const svg = d3
-      .select("body")
-      .append("svg")
-      .attr("height", height)
-      .attr("width", width);
+          const node = svg.append("g")
+                           .attr("stroke", "#fff")
+                           .attr("stroke-width", 1.5)
+                           .selectAll("g")
+                           .data(nodes)
+                           .join("g");
 
-    // Add a line for each link, and a circle for each node.
-    const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-      .selectAll("line")
-      .data(links)
-      .join("line")
-      .attr("stroke-width", d => Math.sqrt(d.value));
+          function clampStr(str, size) {
+            if (!str) {
+              return str;
+            } else if (str.length < size) {
+              return str;
+            } else {
+              return str.slice(0, size);
+            }
+          }
 
-    const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-      .selectAll("g")
-      .data(nodes)
-      .join("g");
+          node.append("circle")
+              .attr("r", d => radiusScale(d.metadata.level))
+              .attr("fill", d => colorScale(d.metadata.level))
+              .append("title")
+              .text(d => d.metadata.title);
 
-    function clampStr(str, size) {
-      if (!str) {
-        return str;
-      } else if (str.length < size) {
-        return str;
-      } else {
-        return str.slice(0, size);
-      }
-    }
+          node.append("text")
+              .attr("dx", d => d.metadata.isSubtree
+                                   ? radiusScale(d.metadata.level) / 2 + 8
+                                   : 40)
+              .attr("dy", "0.35em") // Center text vertically
+              .attr("stroke", "black")
+              .attr("stroke-width", 0.7)
+              .attr("font-weight", 10)
+              .attr("font-size", "12px")
+              .text(d => clampStr(d.metadata.title, 12));
 
-    node.append("circle")
-      .attr("r", d => radiusScale(d.metadata.level))
-      .attr("fill", d => colorScale(d.metadata.level))
-      .append("title")
-      .text(d => d.metadata.title);
+          // Add a drag behavior.
+          node.call(d3.drag()
+                        .on("start", dragstarted)
+                        .on("drag", dragged)
+                        .on("end", dragended));
 
-    node.append("text")
-      .attr("dx", d => d.metadata.isSubtree ? radiusScale(d.metadata.level) / 2 + 8 : 40)
-      .attr("dy", "0.35em")  // Center text vertically
-      .attr("stroke", "black")
-      .attr("stroke-width", 0.7)
-      .attr("font-weight", 10)
-      .attr("font-size", "12px")
-      .text(d => clampStr(d.metadata.title, 12));
+          var maxNodeWidth = 0;
+          node.each(function() {
+            const bbox = this.getBBox();
+            if (bbox.width > maxNodeWidth) {
+              maxNodeWidth = bbox.width;
+            }
+          });
 
-    // Add a drag behavior.
-    node.call(d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended));
+          // Create a simulation with several forces.
+          const simulation =
+              d3.forceSimulation(nodes)
+                  .force("link",
+                         d3.forceLink(links).id(d => d.id).distance(300))
+                  .force("center", d3.forceCenter(width / 2, height / 2))
+                  .force("charge", d3.forceManyBody().strength(-maxNodeWidth))
+                  .force("x", d3.forceX())
+                  .force("y", d3.forceY());
 
-    var maxNodeWidth = 0;
-    node.each(function () {
-      const bbox = this.getBBox();
-      if (bbox.width > maxNodeWidth) {
-        maxNodeWidth = bbox.width;
-      }
-    });
+          // Set the position attributes of links and nodes each time the
+          // simulation ticks.
+          simulation.on("tick", () => {
+            link.attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
 
-    // Create a simulation with several forces.
-    const simulation = d3.forceSimulation(nodes)
-      .force("link", d3
-        .forceLink(links)
-        .id(d => d.id)
-        .distance(300))
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .force("charge", d3.forceManyBody().strength(-maxNodeWidth))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY());
+            node.attr("transform", d => `translate(${d.x},${d.y})`);
+          });
 
+          // Reheat the simulation when drag starts, and fix the subject
+          // position.
+          function dragstarted(event) {
+            if (!event.active)
+              simulation.alphaTarget(0.3).restart();
+            event.subject.fx = event.subject.x;
+            event.subject.fy = event.subject.y;
+          }
 
+          // Update the subject (dragged node) position during drag.
+          function dragged(event) {
+            event.subject.fx = event.x;
+            event.subject.fy = event.y;
+          }
 
-    // Set the position attributes of links and nodes each time the simulation ticks.
-    simulation.on("tick", () => {
-      link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-
-      node
-        .attr("transform", d => `translate(${d.x},${d.y})`);
-    });
-
-    // Reheat the simulation when drag starts, and fix the subject position.
-    function dragstarted(event) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
-    }
-
-    // Update the subject (dragged node) position during drag.
-    function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
-    }
-
-    // Restore the target alpha so the simulation cools after dragging ends.
-    // Unfix the subject position now that it’s no longer being dragged.
-    function dragended(event) {
-      if (!event.active) simulation.alphaTarget(0);
-      event.subject.fx = null;
-      event.subject.fy = null;
-    }
-
-  },
-  function (err) { throw err; }
-);
+          // Restore the target alpha so the simulation cools after dragging
+          // ends. Unfix the subject position now that it’s no longer being
+          // dragged.
+          function dragended(event) {
+            if (!event.active)
+              simulation.alphaTarget(0);
+            event.subject.fx = null;
+            event.subject.fy = null;
+          }
+        },
+        function(err) { throw err; });
--- scripts/visual/indented_subtree.js
+++ scripts/visual/indented_subtree.js
@@ -1,25 +1,19 @@
-import { convertMindMapGraph } from "./utils.js";
+import {convertMindMapGraph} from "./utils.js";
 
 const nodeSize = 10;
 const format = d3.format(",");
 const width = 600;
 
-const margin = ({ top: 20, right: 20, bottom: 20, left: 1000 });
+const margin = ({top : 20, right : 20, bottom : 20, left : 1000});
 
 var root;
 
 const columns = [
-  {
-    label: "Size",
-    value: d => d.value,
-    format,
-    x: 500
-  },
-  {
-    label: "Count",
-    value: d => d.children ? 0 : 1,
-    format: (value, d) => d.children ? format(value) : "-",
-    x: 500 + 120
+  {label : "Size", value : d => d.value, format, x : 500}, {
+    label : "Count",
+    value : d => d.children ? 0 : 1,
+    format : (value, d) => d.children ? format(value) : "-",
+    x : 500 + 120
   }
 ];
 
@@ -69,19 +63,18 @@ function arc(d) {
   return `M${0},${0}A${r},${r} 0,0,${y1 < y2 ? 1 : 0} ${0},${y1 - y2}`;
 }
 
-
 function update() {
   var i = 0;
   root = root.eachBefore(d => d.visible && (d.index = i++));
   console.log("Updating tree for new root")
   // Add links between nodes
   svg.select("g.paths")
-    .attr("fill", "none")
-    .attr("stroke", "#999")
-    .selectAll("path")
-    .data(root.links())
-    .join("path")
-    .attr("d", d => `
+      .attr("fill", "none")
+      .attr("stroke", "#999")
+      .selectAll("path")
+      .data(root.links())
+      .join("path")
+      .attr("d", d => `
 M${d.source.depth * nodeSize + margin.left},${d.source.index * nodeSize}
 V${d.target.index * nodeSize}
 h${nodeSize}
@@ -89,81 +82,84 @@ h${nodeSize}
 
   const nodes = root.descendants();
 
-  const node = svg.select("g.nodes")
-    .selectAll("g")
-    .data(nodes, d => d.d);
+  const node = svg.select("g.nodes").selectAll("g").data(nodes, d => d.d);
 
-  var nodeEnter = node.enter().append("g")
-    .on('click', click)
-    .attr("transform", d => `translate(${margin.left},${d.index * nodeSize})`);
+  var nodeEnter =
+      node.enter()
+          .append("g")
+          .on('click', click)
+          .attr("transform",
+                d => `translate(${margin.left},${d.index * nodeSize})`);
 
   nodeEnter.append("circle")
-    .attr("cx", d => d.depth * nodeSize)
-    .attr("r", 2.5)
-    .style("display", d => d.visible ? "inline" : "none")
-    .attr("fill", d => d.children ? null : "#999");
+      .attr("cx", d => d.depth * nodeSize)
+      .attr("r", 2.5)
+      .style("display", d => d.visible ? "inline" : "none")
+      .attr("fill", d => d.children ? null : "#999");
 
   nodeEnter.append("text")
-    .attr("dy", "0.32em")
-    .attr("x", d => (d.depth * nodeSize + 6))
-    .style("display", d => d.visible ? "inline" : "none")
-    .text(getTitle);
+      .attr("dy", "0.32em")
+      .attr("x", d => (d.depth * nodeSize + 6))
+      .style("display", d => d.visible ? "inline" : "none")
+      .text(getTitle);
 
   // Append a 'path' element for each target link
-  nodeEnter.each(function (d) {
+  nodeEnter.each(function(d) {
     d.targetLinks.forEach(link => {
-      function linkId() {
-        return `path${link.source.id}--${link.target.id}`;
-      }
+      function linkId() { return `path${link.source.id}--${link.target.id}`; }
 
       d3.select(this)
-        .append("path")
-        .attr("d", arc(link))
-        .attr("id", linkId())
-        .style("fill", "none")
-        .style("stroke", "#999")
-        .style("stroke-width", "1.5px");
+          .append("path")
+          .attr("d", arc(link))
+          .attr("id", linkId())
+          .style("fill", "none")
+          .style("stroke", "#999")
+          .style("stroke-width", "1.5px");
 
       d3.select(this)
-        .append("text")
-        .append("textPath") //append a textPath to the text element
-        .attr("xlink:href", (d, i) => "#" + linkId()) //place the ID of the path here
-        .style("text-anchor", "middle") //place the text halfway on the arc
-        .attr("startOffset", "50%")
-        .text(`${link.source.data.name}:${link.source.data.loc.line} -> ${link.target.data.name}:${link.target.data.loc.line}`); // the text to display
+          .append("text")
+          .append("textPath") // append a textPath to the text element
+          .attr("xlink:href",
+                (d, i) => "#" + linkId()) // place the ID of the path here
+          .style("text-anchor", "middle") // place the text halfway on the arc
+          .attr("startOffset", "50%")
+          .text(`${link.source.data.name}:${link.source.data.loc.line} -> ${
+              link.target.data.name}:${
+              link.target.data.loc.line}`); // the text to display
     });
   });
 
-  nodeEnter.append("title")
-    .text(d => d.ancestors().reverse().map(d => d.data.name).join("/"));
+  nodeEnter.append("title").text(
+      d => d.ancestors().reverse().map(d => d.data.name).join("/"));
 
-  for (const { label, value, format, x } of columns) {
+  for (const {label, value, format, x} of columns) {
     svg.append("text")
-      .attr("dy", "0.32em")
-      .attr("y", -nodeSize)
-      .attr("x", x + margin.left)
-      .attr("text-anchor", "end")
-      .attr("font-weight", "bold")
-      .text(label);
+        .attr("dy", "0.32em")
+        .attr("y", -nodeSize)
+        .attr("x", x + margin.left)
+        .attr("text-anchor", "end")
+        .attr("font-weight", "bold")
+        .text(label);
 
     nodeEnter.append("text")
-      .attr("dy", "0.32em")
-      .attr("x", x + margin.left)
-      .attr("text-anchor", "end")
-      .attr("fill", d => d.children ? null : "#555")
-      .data(root.copy().sum(value).descendants())
-      .text(d => format(d.value, d));
+        .attr("dy", "0.32em")
+        .attr("x", x + margin.left)
+        .attr("text-anchor", "end")
+        .attr("fill", d => d.children ? null : "#555")
+        .data(root.copy().sum(value).descendants())
+        .text(d => format(d.value, d));
   }
 
   node.exit().remove()
 }
 
-const svg = d3.select("body").append("svg")
-  .attr("width", 1200)
-  .attr("height", 12000)
-  .attr("font-family", "sans-serif")
-  .attr("font-size", 10)
-  .style("overflow", "visible");
+const svg = d3.select("body")
+                .append("svg")
+                .attr("width", 1200)
+                .attr("height", 12000)
+                .attr("font-family", "sans-serif")
+                .attr("font-size", 10)
+                .style("overflow", "visible");
 
 svg.append("g").attr("class", "paths");
 svg.append("g").attr("class", "nodes");
@@ -171,8 +167,7 @@ svg.append("g").attr("class", "nodes");
 function getInitialVisibility(d) {
   var visible = true;
   if (d.data.isSubtree && 0 < d.ancestors().length) {
-    search_loop:
-    for (parent of d.ancestors()) {
+    search_loop: for (parent of d.ancestors()) {
       if (parent.data.isSubtree && parent != d) {
         for (const prop of parent.data.properties) {
           if (prop.data.kind == "Visibility") {
@@ -183,7 +178,6 @@ function getInitialVisibility(d) {
           }
         }
       }
-
     }
   }
 
@@ -193,7 +187,7 @@ function getInitialVisibility(d) {
 function onLoadAll(graphData, treeData) {
   var counter = 0;
   var idx = 0;
-  root = d3.hierarchy(treeData, d => d.subtrees).eachBefore(function (d) {
+  root = d3.hierarchy(treeData, d => d.subtrees).eachBefore(function(d) {
     d.visible = getInitialVisibility(d);
     d.id = String(d.data.id);
     d.sourceLinks = [];
@@ -205,16 +199,16 @@ function onLoadAll(graphData, treeData) {
   const treeIdToNode = new Map(root.descendants().map(d => [d.id, d]));
 
   console.log(treeIdToNode);
-  const links = base_links.map(({ source, target, value }) => ({
-    source: treeIdToNode.get(String(source)),
-    target: treeIdToNode.get(String(target)),
-    value
-  }));
+  const links = base_links.map(({source, target, value}) => ({
+                                 source : treeIdToNode.get(String(source)),
+                                 target : treeIdToNode.get(String(target)),
+                                 value
+                               }));
 
   console.log(links);
 
   for (const link of links) {
-    const { source, target, value } = link;
+    const {source, target, value} = link;
     source.sourceLinks.push(link);
     target.targetLinks.push(link);
   }
@@ -222,17 +216,11 @@ function onLoadAll(graphData, treeData) {
   update();
 }
 
-d3.json("/tmp/mindmap_graph.json").then(
-  function (graphData) {
-    d3.json("/tmp/subtree-hierarhcy.json").then(
-      function (treeData) {
-        onLoadAll(graphData, treeData);
-      },
-      function (err) {
-        throw err;
-      }
-    );
-  },
-  function (err) { throw err; }
-);
-
+d3.json("/tmp/mindmap_graph.json")
+    .then(
+        function(graphData) {
+          d3.json("/tmp/subtree-hierarhcy.json")
+              .then(function(treeData) { onLoadAll(graphData, treeData); },
+                    function(err) { throw err; });
+        },
+        function(err) { throw err; });
