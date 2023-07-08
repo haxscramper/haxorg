5ccc306040ec33a2a788eea852687d4f427329d3
haxscramper <haxscramper@gmail.com>
2023-07-08 10:09:59 +0400

    Revert changes to json exporter

--- src/exporters/ExporterJson.hpp
+++ src/exporters/ExporterJson.hpp
@@ -12,25 +12,101 @@ struct ExporterJson : public Exporter<ExporterJson, json> {
     void visitSubtreeValueFields(json&, In<sem::Subtree> tree);
 
     template <typename T>
-    json newRes(CR<T>);
+    json newRes(CVec<T>) {
+        return json::array();
+    }
+
 
     template <typename T>
-    json visit(CR<T>);
+    json newRes(CR<T>) {
+        return json::object();
+    }
+
 
     template <typename T>
-    void visitField(json& j, const char* name, CR<Opt<T>> value);
+    json newRes(CR<Opt<T>> arg) {
+        if (arg) {
+            return newRes(*arg);
+        } else {
+            return json();
+        }
+    }
+
+    template <typename... Args>
+    json newRes(CR<Variant<Args...>> var) {
+        return std::visit(
+            [this](auto const& it) -> json { return visit(it); }, var);
+    }
+
+    json visit(CR<Str> value) { return json(value); }
+    json visit(CR<QString> value) { return json(value); }
+    json visit(CR<bool> value) { return json(value); }
+    json visit(CR<int> value) { return json(value); }
+
+    json visit(CR<QDate> value) {
+        return json(value.toString(Qt::ISODate).toStdString());
+    }
+
+    json visit(CR<QTime> value) {
+        return json(value.toString(Qt::ISODate).toStdString());
+    }
+
+    json visit(CR<QDateTime> value) {
+        return json(value.toString(Qt::ISODate).toStdString());
+    }
+
+
+    template <typename E>
+    json visit(E value)
+        requires(std::is_enum<E>::value)
+    {
+        return json(to_string(value).toStdString());
+    }
 
     template <typename T>
-    json visit(CR<Vec<T>> values);
+    json visit(CR<T> arg)
+        requires(!std::is_enum<T>::value)
+    {
+        json tmp = _this()->newRes(arg);
+        _this()->visit(tmp, arg);
+        return tmp;
+    }
+
+    json newRes(sem::SemId org);
 
     template <typename T>
-    json visit(CR<UnorderedMap<Str, T>> map);
+    void visitField(json& j, const char* name, CR<Opt<T>> value) {
+        if (value) {
+            j[name] = visit(value.value());
+        } else {
+            j[name] = json();
+        }
+    }
 
     template <typename T>
-    void visitField(json& j, const char* name, CR<T> field);
+    json visit(CR<Vec<T>> values) {
+        json tmp = json::array();
+        for (const auto& it : values) {
+            tmp.push_back(visit(it));
+        }
+        return tmp;
+    }
 
-    void visitDocument(json& j, In<sem::Document> doc);
+    template <typename T>
+    json visit(CR<UnorderedMap<Str, T>> map) {
+        json tmp = json::object();
+        for (const auto& [key, val] : map) {
+            tmp[key.toStdString()] = visit(val);
+        }
+        return tmp;
+    }
+
+    template <typename T>
+    void visitField(json& j, const char* name, CR<T> field) {
+        j[name] = visit(field);
+    }
+
+    void visitDocument(json& j, In<sem::Document> doc) {
+        visitField(j, "subnodes", doc->subnodes);
+    }
 };
-
-extern template class Exporter<ExporterJson, json>;
-extern class ExporterJson;
