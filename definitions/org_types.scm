4695a103026034e1fb9f32f461b365693ba57953
haxscramper <haxscramper@gmail.com>
2023-07-16 20:22:03 +0400

    Define macro to build type definition DSL

--- definitions/org_types.scm
+++ definitions/org_types.scm
@@ -1,22 +1,73 @@
 (use-modules (oop goops))
+(use-modules (srfi srfi-1))
+(use-modules (language tree-il))
+(use-modules (ice-9 pretty-print))
 
-(define-class <type> ()
-  (name #:init-keyword #:name)
-  (brief-doc #:init-keyword #:brief-doc)
-  (members #:init-keyword #:members #:init-value '())
-  (bases #:init-keyword #:bases #:init-value '())
-  (long-doc #:init-keyword #:long-doc #:init-value ""))
+(define (is-positional field)
+  (eq? (length field) 1))
 
-(define* (d:struct name brief-doc
-                   #:key
-                   (members '())
-                   (bases '())
-                   (long-doc ""))
-  (make <type>
-    #:name name
-    #:brief-doc brief-doc
-    #:members members
-    #:bases bases
-    #:long-doc long-doc))
+(define (get-positional-defs field-defs)
+  (remove (lambda (field) (not (is-positional field))) field-defs))
+
+(define (get-optional-defs field-defs)
+  (remove (lambda (field) (is-positional field)) field-defs))
+
+(define (get-positional-fields field-defs)
+  (map (lambda (field-def)
+         (let* ((field-name (car field-def))
+                (field-kwd (symbol->keyword field-name)))
+           `(,field-name #:init-keyword ,field-kwd)))
+       (get-positional-defs field-defs)))
+
+(define (get-optional-fields field-defs)
+  (map (lambda (field-def)
+         (let* ((field-name (car field-def))
+                (field-kwd (symbol->keyword field-name))
+                (default-value (cdr field-def)))
+           `(,field-name
+             #:init-keyword ,field-kwd
+             #:init-form ,default-value)))
+       (get-optional-defs field-defs)))
+
+(define-macro (simple-define-type type-name make-fn-name . field-defs)
+  (let* ((positional-fields (get-positional-fields field-defs))
+         (optional-fields (get-optional-fields field-defs))
+         (positional-args
+          (map (lambda (field-def) (car field-def))
+               (get-positional-defs field-defs)))
+         (pass-args
+          (fold
+           (lambda (prev next) (append prev next))
+           (list)
+           (map (lambda (field-def)
+                  (list (symbol->keyword (car field-def)) (car field-def)))
+                field-defs)))
+         (optional-args
+          (map (lambda (field-def) (cons (car field-def) (cdr field-def)))
+               (get-optional-defs field-defs))))
+    `(begin
+       (define-class ,type-name ()
+         ,@positional-fields
+         ,@optional-fields)
+       (define* (,make-fn-name ,@positional-args #:key ,@optional-args)
+         (make ,type-name ,@pass-args)))))
+
+;; (pretty-print
+;;  (tree-il->scheme
+;;   (macroexpand
+;;    '()
+;;    )))
+
+(simple-define-type
+ <type> d:struct
+ (name)
+ (brief-doc)
+ (members (list))
+ (bases (list))
+ (long-doc ""))
+
+(simple-define-type
+ <group> d:group
+ (items))
 
 (d:struct 'Org "Base class for org mode types" #:bases '(Some))
