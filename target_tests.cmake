ffe47a70d3ed1d65960cd7a3f15db0ece8c42f24
haxscramper <haxscramper@gmail.com>
2023-08-03 21:58:07 +0400

    test: move corpus test driver logic in a separate class

--- target_tests.cmake
+++ target_tests.cmake
@@ -3,6 +3,6 @@ target_link_libraries(tests PUBLIC haxorg)
 set_common_files(tests)
 set_target_output(tests)
 set_target_flags(tests)
-glob_add_sources(tests "t*.cpp" "${BASE}/tests/.*")
+glob_add_sources2(tests "${BASE}/tests/.*")
 target_link_libraries(tests PUBLIC fmt::fmt yaml-cpp::yaml-cpp haxorg gtest
                                    Qt6::Core Qt6::Gui perfetto)
