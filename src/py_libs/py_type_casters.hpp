7951488b97a99c14acb9fb5a419f0e2b2d484235
haxscramper <haxscramper@gmail.com>
2023-10-03 23:27:47 +0400

    Add `Str` type caster

--- src/py_libs/py_type_casters.hpp
+++ src/py_libs/py_type_casters.hpp
@@ -4,6 +4,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <hstd/stdlib/algorithms.hpp>
+#include <hstd/stdlib/Str.hpp>
 
 
 namespace PYBIND11_NAMESPACE {
@@ -12,6 +13,43 @@ namespace detail {
     struct type_caster<QString> {
       public:
         PYBIND11_TYPE_CASTER(QString, _("str"));
+
+        bool load(handle src, bool) {
+
+            /* Extract PyObject from handle */
+            PyObject* source = src.ptr();
+
+            if (!PyUnicode_Check(source)) {
+                return false;
+            }
+
+            Py_ssize_t  size;
+            const char* ptr = PyUnicode_AsUTF8AndSize(source, &size);
+
+            if (!ptr) {
+                return false;
+            }
+
+            /* Now try to convert into a C++ int */
+            value = QString::fromUtf8(ptr, size);
+
+            /* Ensure return code was OK (to avoid out-of-range errors etc)
+             */
+            return (!PyErr_Occurred());
+        }
+
+        static handle cast(
+            QString src,
+            return_value_policy /* policy */,
+            handle /* parent */) {
+            return (PyUnicode_FromString(src.toUtf8().data()));
+        }
+    };
+
+    template <>
+    struct type_caster<Str> {
+      public:
+        PYBIND11_TYPE_CASTER(Str, _("str"));
 
         bool load(handle src, bool) {
 
