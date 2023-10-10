7294cd54c572b59d6b5a0483de4ddf7713cfe017
haxscramper <haxscramper@gmail.com>
2023-10-10 20:53:37 +0400

    Add simple benchmark target for cmake

---
+++ src/bench/main.cpp
@@ -0,0 +1,40 @@
+#include <sem/SemOrg.hpp>
+#include <sem/SemConvert.hpp>
+
+#include <benchmark/benchmark.h>
+
+static void impl() {
+    qDebug() << "Running bench";
+    OrgTokenGroup       tokens;
+    SPtr<OrgTokenizer>  tokenizer = OrgTokenizer::initImpl(&tokens, false);
+    OrgNodeGroup        nodes{nullptr};
+    QString             base;
+    Lexer<OrgTokenKind> lex{&tokens};
+    SPtr<OrgParser>     parser = OrgParser::initImpl(&nodes, false);
+    UPtr<OrgSpec>       spec;
+    nodes.tokens = &tokens;
+
+    base = readFile(QFileInfo("/home/haxscramper/tmp/doc.org"));
+
+    Func<LineCol(CR<PosStr>)> locationResolver =
+        [&](CR<PosStr> str) -> LineCol {
+        return {0, 0, 0};
+    };
+
+    tokenizer->setLocationResolver(locationResolver);
+    parser->setLocationResolver(locationResolver);
+
+    tokens.base = base.data();
+    PosStr str{base};
+    tokenizer->lexGlobal(str);
+    qDebug() << "Completed run";
+}
+
+static void BenchLexFile(benchmark::State& state) { impl(); }
+
+
+QTextStream qcout;
+
+int main() { impl(); }
+// BENCHMARK(BenchLexFile);
+// BENCHMARK_MAIN();
---
+++ src/cmake/target_bench.cmake
@@ -0,0 +1,8 @@
+add_executable(bench)
+target_link_libraries(bench PUBLIC haxorg)
+set_common_files(bench)
+set_target_output(bench)
+set_target_flags(bench)
+glob_add_sources2(bench "${BASE}/src/bench/.*")
+target_link_libraries(bench PUBLIC fmt::fmt yaml-cpp::yaml-cpp haxorg gtest
+                                   Qt6::Core Qt6::Gui perfetto benchmark::benchmark_main)
