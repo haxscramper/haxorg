2197959b7b12b05207d2a8fc7856756682185224
haxscramper <haxscramper@gmail.com>
2023-10-24 23:20:24 +0400

    Add script to generate re/flex rules
    Lexer rule  definitions, unless  overloaded with the  custom logic,  can be
    seen as  a pure data  about the  language structure. While  some imperative
    logic needs to be added to  allow for transitions of different lexer modes,
    otherwise it  does not make a  lot of sense  to encode this knowledge  in a
    format that it can never be  retrieved from later on (for string generation
    fuzzing,   targeting  other   lexer  generation,   boilerplate  automation,
    simplified documentation maintenance and refactoring.)

---
+++ src/base_lexer/base_lexer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import yaml
+from yaml.loader import SafeLoader
+from pydantic import BaseModel, validator
+from typing import List, Optional, Union
+import os
+
+class Action(BaseModel):
+    do: str
+    to: Optional[str]
+    from_: Optional[str] = None
+
+    # Convert "from" from the YAML to "from_" in our model
+    @validator("from_", pre=True, always=True)
+    def from_field(cls, v, values):
+        return values.get('from')
+
+class Rule(BaseModel):
+    re: str
+    token: str
+    states: Optional[List[str]] = None
+    actions: List[Action]
+    line: Optional[int]
+
+class State(BaseModel):
+    name: str
+    kind: str
+    line: Optional[int]
+
+class Configuration(BaseModel):
+    states: List[State]
+    rules: List[Rule]
+
+
+class SafeLineLoader(SafeLoader):
+    def construct_mapping(self, node, deep=False):
+        mapping = super(SafeLineLoader, self).construct_mapping(node, deep=deep)
+        mapping['__line__'] = node.start_mark.line + 1
+        return mapping
+
+def parse_yaml_to_pydantic(file_path: str) -> Configuration:
+    with open(file_path, 'r') as f:
+        data = yaml.load(f, Loader=SafeLineLoader)
+        
+    # Process line information for sub-lists
+    for state in data["states"]:
+        state["line"] = state.pop("__line__", None)
+
+    for rule in data["rules"]:
+        rule["line"] = rule.pop("__line__", None)
+
+    return Configuration(**data)
+
+MATCH_WIDTH = 40
+
+# Convert rule data to re/flex code pattern
+def rule_to_reflex_code(rule: Rule) -> str:
+    state_prefix = ""
+    if rule.states:
+        state_prefix = "<{}>".format(",".join(rule.states))
+
+    state_prefix = (state_prefix + rule.re).ljust(MATCH_WIDTH)
+
+    actions = []
+
+    for action in rule.actions:
+        if action.do == "push":
+            actions.append(f"push_state({action.to});")
+        elif action.do == "pop":
+            actions.append(f"pop_expect({action.from_}, {action.to});")
+
+    actions_code = " ".join(actions)
+    return f"{state_prefix} {{ add(BaseTokenKind::{rule.token}); {actions_code} }}"
+
+def generate_reflex_code(config: Configuration) -> str:
+    return "\n".join([rule_to_reflex_code(rule) for rule in config.rules])
+
+# Example usage:
+config = parse_yaml_to_pydantic(os.path.join(os.path.dirname(os.path.abspath(__file__)), "base_lexer.yaml"))
+for rule in config.rules:
+    print(rule_to_reflex_code(rule))
