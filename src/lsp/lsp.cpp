694ce8097d9c708cd095cf0dba42845a9798d4d9
haxscramper <haxscramper@gmail.com>
2023-11-03 00:20:11 +0400

    Use boost::beast for LSP HTTP handling

--- src/lsp/lsp.cpp
+++ src/lsp/lsp.cpp
@@ -10,10 +10,16 @@
 #include <absl/log/log_sink_registry.h>
 #include <absl/log/initialize.h>
 #include <absl/log/internal/globals.h>
+#include <boost/beast/http.hpp>
+#include <boost/beast/core.hpp>
 #include <fstream>
 
-namespace asio = boost::asio;
+namespace asio  = boost::asio;
+namespace beast = boost::beast; // from <boost/beast.hpp>
+namespace http  = beast::http;  // from <boost/beast/http.hpp>
+
 using asio::ip::tcp;
+
 
 class Session : public std::enable_shared_from_this<Session> {
   public:
@@ -21,19 +27,6 @@ class Session : public std::enable_shared_from_this<Session> {
 
     tcp::socket& get_socket() { return socket; }
 
-    void start() {
-        LOG(INFO) << "Started session";
-        asio::async_read_until(
-            socket,
-            asio::dynamic_buffer(data),
-            "\r\n\r\n",
-            boost::bind(
-                &Session::handle_read,
-                this,
-                shared_from_this(),
-                asio::placeholders::error,
-                asio::placeholders::bytes_transferred));
-    }
 
     json build_response(const json& request) {
         // Example: Check for completion request and send a
@@ -52,58 +45,44 @@ class Session : public std::enable_shared_from_this<Session> {
     }
 
     void handle_raw_request(std::size_t length) {
-        // Parse received data with nlohmann::json
-        // TODO use string view
-        std::string jsonStr  = data.substr(length);
-        json        request  = json::parse(jsonStr);
-        json        response = build_response(request);
-
-        std::string responseStr = response.dump();
-        std::string headers     = "Content-Length: "
-                            + std::to_string(responseStr.size())
-                            + "\r\n\r\n";
-        socket.send(asio::buffer(headers + responseStr));
-
-        // Clear buffer and wait for another message
-        data.clear();
-        start();
-    }
-
-    void handle_read(
-        std::shared_ptr<Session>&        s,
-        const boost::system::error_code& ec,
-        size_t                           length) {
-        if (!ec) {
-            DLOG(INFO) << "Received request to read LSP";
-            // Extract Content-Length from headers
-            std::string headers = data.substr(0, length);
-            std::size_t pos     = headers.find("Content-Length: ");
-            if (pos == std::string::npos) {
-                DLOG(ERROR)
-                    << "Missing content-length header in the input";
-                // Error: Missing Content-Length header
-                return;
-            }
-            std::size_t contentLength = std::stoi(
-                headers.substr(pos + 16));
-            data.clear();
-
-            // Read the JSON body of the message
-            asio::async_read(
-                socket,
-                asio::dynamic_buffer(data),
-                asio::transfer_exactly(contentLength),
-                [this, length](boost::system::error_code ec, std::size_t) {
-                    if (!ec) {
-                        handle_raw_request(length);
-                    }
-                });
+        LOG(INFO) << "Handling raw request" << request.body();
+        try {
+            json requestJson  = json::parse(request.body());
+            json responseJson = build_response(requestJson);
+            response.body()   = responseJson.dump();
+            response.result(http::status::ok);
+            response.set(http::field::content_type, "text/json");
+        } catch (json::parse_error& err) {
+            LOG(ERROR) << "Request handling contained invalid JSON "
+                       << err.what();
+            response.result(http::status::bad_request);
+            response.body() = err.what();
         }
     }
 
+    void wait_for_request() {
+        LOG(INFO) << "Started session";
+        auto self(shared_from_this());
+        http::async_read(
+            socket,
+            buffer,
+            request,
+            [self](boost::system::error_code ec, std::size_t length) {
+                LOG(INFO) << "Received input request";
+                self->handle_read(ec, length);
+            });
+    }
+
+
+    void handle_read(const boost::system::error_code& ec, size_t length) {
+        handle_raw_request(length);
+    }
+
   private:
-    tcp::socket socket;
-    std::string data;
+    tcp::socket                       socket;
+    http::request<http::string_body>  request;
+    http::response<http::string_body> response;
+    beast::flat_buffer                buffer{8192};
 };
 
 class Server {
@@ -126,7 +105,7 @@ class Server {
         const boost::system::error_code& err) {
         if (!err) {
             LOG(INFO) << "Accepted connection and created session";
-            session->start();
+            session->wait_for_request();
             session = std::make_shared<Session>(ios);
             acceptor.async_accept(
                 session->get_socket(),
