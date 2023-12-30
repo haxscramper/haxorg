3582cb3c4f7cfe19c1b88be8f5f6bc613437b5d9
haxscramper <haxscramper@gmail.com>
2023-12-30 14:28:41 +0400

    repo: import forensics db tables to DF for test

--- tests/python/repo/test_code_forensics.py
+++ tests/python/repo/test_code_forensics.py
@@ -4,23 +4,66 @@ from tempfile import NamedTemporaryFile, TemporaryDirectory, mktemp
 from plumbum import local
 from py_scriptutils.files import get_haxorg_repo_root_path
 from beartype import beartype
-from beartype.typing import Dict
+from beartype.typing import Dict, Optional, Type
+from types import TracebackType
 from py_scriptutils.toml_config_profiler import merge_dicts
 from pathlib import Path
+from datetime import datetime
+from dataclasses import dataclass
+import sqlite3
+import pandas as pd
+import shutil
+
+from rich.console import Console
+from rich.table import Column, Table, Style
+from rich import box
+
 
 @beartype
-def get_git(dir: str):
-    assert Path(dir).exists()
+def print_df_rich(df: pd.DataFrame, title: str):
+    console = Console()
+    table = Table(show_lines=False,
+                  box=box.ASCII,
+                  padding=(0, 1),
+                  show_edge=False,
+                  title=f"[red]{title}[/red]",
+                  title_justify="left")
+
+    # Add columns to the table
+    for column in df.columns:
+        table.add_column(str(column))
+
+    # Add rows to the table, with specific formatting
+    for index, row in df.iterrows():
+        row_data = []
+        for item in row:
+            if isinstance(item, (int, float)):
+                # Format numbers with cyan color
+                row_data.append(f"[cyan]{item}[/cyan]")
+            elif isinstance(item, str):
+                # Quote strings and highlight in yellow
+                row_data.append(f"[yellow]\"{item}\"[/yellow]")
+            else:
+                row_data.append(str(item))
+        table.add_row(*row_data)
+
+    console.print(table)
+
+
+@beartype
+def get_git(dir: Path):
+    assert dir.exists()
     git = local["git"].with_cwd(dir)
-    return git 
+    return git
+
 
 @beartype
-def git_init_repo(dir: str):
+def git_init_repo(dir: Path):
     get_git(dir).run(("init"))
 
 
 @beartype
-def git_write_files(dir: str, files: Dict[str, str]):
+def git_write_files(dir: Path, files: Dict[str, str]):
     for path, content in files.items():
         path = Path(dir).joinpath(path)
         if not path.parent.exists():
@@ -31,39 +74,86 @@ def git_write_files(dir: str, files: Dict[str, str]):
 
 
 @beartype
-def git_add(dir: str):
+def git_add(dir: Path):
     get_git(dir).run(("add", "."))
 
-@beartype
-def git_commit(dir: str, message: str):
-    get_git(dir).run(("commit", "-m", message))   
 
 @beartype
-def run_forensics(dir: str, params: dict):
-    run = local[str(get_haxorg_repo_root_path().joinpath("build/haxorg/scripts/cxx_repository/code_forensics"))]
-    params = merge_dicts([params, {
-        "repo": {
-            "path": dir,
-            "branch": "master"
-        }
-    }])
+def git_commit(dir: Path,
+               message: str,
+               date: datetime = datetime.now(),
+               author: str = "haxorg-test-author",
+               email="haxorg-test-email@email.com"):
+    git = get_git(dir)
+    git = git.with_env(GIT_AUTHOR_DATE=date.strftime("%Y-%m-%d %H:%M:%S"),
+                       GIT_COMMITTER_DATE=date.strftime("%Y-%m-%d %H:%M:%S"))
+    git.run(("commit", f"--author={author} <{email}>", "-m", message))
+
+
+@beartype
+def run_forensics(dir: Path, params: dict):
+    run = local[str(get_haxorg_repo_root_path().joinpath(
+        "build/haxorg/scripts/cxx_repository/code_forensics"))]
+    params = merge_dicts([params, {"repo": {"path": str(dir), "branch": "master"}}])
 
     run.run((json.dumps(params)))
 
 
+@beartype
+@dataclass
+class GitTestRepository:
+    startFiles: Dict[str, str]
+    db: Optional[str] = ""
+    dir: Optional[TemporaryDirectory | Path] = None
+    initMessage: str = "init"
+    fixedDir: Optional[str] = None
+
+    def git_dir(self) -> Path:
+        return self.dir if isinstance(self.dir, Path) else Path(self.dir.name)
+
+    def __enter__(self):
+        if self.fixedDir:
+            self.dir = Path(self.fixedDir)
+            if self.dir.exists():
+                shutil.rmtree(self.dir)
+
+            self.dir.mkdir()
+
+        else:
+            self.dir = TemporaryDirectory()
+            self.dir.__enter__()
+
+        git_init_repo(self.git_dir())
+        git_write_files(self.git_dir(), self.startFiles)
+        git_add(self.git_dir())
+        git_commit(self.git_dir(), self.initMessage)
+        self.db = mktemp(suffix=".sqlite")
+        return self
+
+    def __exit__(self, exc_type: Optional[Type[BaseException]],
+                 exc_value: Optional[BaseException],
+                 traceback: Optional[TracebackType]) -> Optional[bool]:
+        self.dir.__exit__(exc_type, exc_value, traceback)
+        if exc_type is not None:
+            return False
+        
+        db = Path(self.db)
+        if db.exists() and not self.fixedDir:
+            db.unlink()
+
 
 def test_can_run_dir():
-    with TemporaryDirectory() as dir:
-        git_init_repo(dir)
-        git_write_files(dir, {"a": "b"})
-        git_add(dir)
-        git_commit(dir, "init")
-        db = mktemp(suffix=".sqlite")
-        assert not Path(db).exists(), db
-        run_forensics(dir, {
-            "out": {
-                "db_path": db
-            }        
-        })
+    with GitTestRepository({"a": "b"}, fixedDir="/tmp/fixed_git_dir_1") as repo:
+        assert not Path(repo.db).exists(), repo.db
+        run_forensics(repo.git_dir(), {"out": {"db_path": str(repo.db)}})
+        assert Path(repo.db).exists(), repo.db
 
-        assert Path(db).exists(), db
+        conn = sqlite3.connect(repo.db)
+
+        tables = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';",
+                                   conn)
+
+        print("")
+        for table_name in tables['name'].tolist():
+            df = pd.read_sql_query(f"SELECT * from {table_name}", conn)
+            print_df_rich(df, title=table_name)
