a2547ff7a450560bc61a8681535ac68f1a5f85ae
haxscramper <haxscramper@gmail.com>
2024-01-20 14:32:59 +0400

    py: gtest runner, organize tests into classes

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -4,6 +4,8 @@ from py_scriptutils.repo_files import get_haxorg_repo_root_path
 import subprocess
 import re
 from beartype.typing import Tuple, Optional
+from beartype import beartype
+from dataclasses import dataclass
 
 from py_scriptutils.tracer import TraceCollector
 
@@ -60,22 +62,62 @@ def pytest_collect_file(parent, path):
         return GTestFile.from_parent(parent, fspath=path)
 
 
-class GTestFile(pytest.File):
-    def collect(self):
-        for test in parse_google_tests(binary_path):
-            def prototype(options):
-                pass
+@beartype
+@dataclass
+class GTestParams():
+    class_name: str
+    test_name: str
 
-            yield GTestItem.from_parent(self, name=re.sub(r"[\./]", "_", test), callobj=prototype)
+    def fullname(self):
+        return f"{self.class_name}.{self.test_name}"
+
+
+class GTestClass(pytest.Class):
+    def __init__(self, name, parent):
+        super().__init__(name, parent)
+        self.tests: list[GTestParams] = []
+
+    def add_test(self, test: GTestParams):
+        self.tests.append(test)
+
+    def collect(self):
+        for test in self.tests:
+            yield GTestItem.from_parent(self, gtest=test, name=test.test_name, callobj=lambda: None)
+
+    def _getobj(self):
+        # Return a dummy class object
+        return type(self.name, (object,), {})
+
 
 
 class GTestItem(pytest.Function):
+    def __init__(self, gtest: GTestParams, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gtest = gtest
+
     def runtest(self):
-        subprocess.run([binary_path, f"--gtest_filter={self.name}"])
+        subprocess.run([binary_path, f"--gtest_filter={self.gtest.fullname()}"])
 
     @property
     def location(self) -> Tuple[str, Optional[int], str]:
         # vscode python plugin has a check for `if testfunc and fullname != testfunc + parameterized:`
-        return ("", None, self.name)
+        return ("", None, self.gtest.fullname())
 
+    def _getobj(self):
+        # Return a dummy function
+        return lambda: None
+
+class GTestFile(pytest.Module):
+    def collect(self):
+        class_tests = {}
+        for test in parse_google_tests(binary_path):
+            class_name, test_name = test.split('.', 1)
+            class_tests.setdefault(class_name, []).append(test_name)
+
+        for class_name, tests in class_tests.items():
+            gtest_class = GTestClass.from_parent(self, name=class_name)
+            for test_name in tests:
+                gtest_class.add_test(GTestParams(class_name, test_name))
+
+            yield gtest_class
 
