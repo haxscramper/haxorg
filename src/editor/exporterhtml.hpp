1fbd6da0e289c96844382838d72f8b4e46eafec4
haxscramper <haxscramper@gmail.com>
2024-02-13 23:05:47 +0400

    editor: use lexbor and HTML exporter
    =QTextDocument= does not  provide enough tools to cleanly  map the incoming
    rich text to  the org node structure, exporter to  the =QTextDocument= also
    does not  provide enough control over  the resulting text structure  as the
    whole thing is written as a stream of text fragments.
    
    Directly  generating  HTML results  provides  a  simpler logic  of  mapping
    between org-mode and what is displayed in the QML side.

---
+++ src/editor/exporterhtml.hpp
@@ -0,0 +1,149 @@
+#ifndef EXPORTERHTML_HPP
+#define EXPORTERHTML_HPP
+
+#include <exporters/Exporter.hpp>
+#include <hstd/wrappers/textlayouter.hpp>
+#include <concepts>
+
+
+struct ExporterHtml : public Exporter<ExporterHtml, layout::BlockId> {
+    using Base = Exporter<ExporterHtml, layout::BlockId>;
+#define __ExporterBase Base
+    EXPORTER_USING()
+#undef __ExporterBase
+
+
+    using Res    = layout::BlockId;
+    using LytStr = layout::LytStr;
+
+    layout::SimpleStringStore store;
+    layout::BlockStore        b;
+
+    ExporterHtml() : store{&b} {}
+    Res newRes(CR<sem::SemId<sem::Org>> id) { return Res::Nil(); }
+    Res string(Str const& str) { return b.text(store.str(str)); }
+
+    void visit(Res& res, sem::SemId<sem::Org> org) {
+        visitDispatch(res, org);
+
+        if (res.isNil()) {
+            res = string(
+                "TODO in convert visit [" + fmt1(org->getKind()) + "]");
+        }
+    }
+
+    Res eval(sem::SemId<sem::Org> org) {
+        Res tmp = Res::Nil();
+        visit(tmp, org);
+        return tmp;
+    }
+
+    template <typename T>
+    Res eval(CR<T> it) {
+        Res tmp = Res::Nil();
+        visit(tmp, it);
+
+        if (tmp.isNil()) {
+            tmp = string("TODO " + demangle(typeid(it).name()));
+        }
+
+        return tmp;
+    }
+
+    template <sem::NotOrg T>
+    void visit(Res& res, CR<T> it) {
+        res = string(
+            demangle(typeid(it).name())
+            + " visit of not derived from org");
+    }
+
+
+    Res multiString(Str const& str) {
+        Res res = b.stack();
+        for (const auto& line : str.split("\n")) {
+            b.add_at(res, string(line));
+        }
+
+        return res;
+    }
+
+    template <typename T>
+    void visitField(Res& res, char const* name, CR<T> it) {}
+
+    Res stackSubnodes(sem::SemId<sem::Org> doc) {
+        Res res = b.stack();
+        for (const auto& it : doc->subnodes) { b.add_at(res, eval(it)); }
+        return res;
+    }
+
+    Res lineSubnodes(sem::SemId<sem::Org> doc) {
+        Res res = b.line();
+        for (const auto& it : doc->subnodes) { b.add_at(res, eval(it)); }
+        return res;
+    }
+
+    Res directionWrap(bool isStack, Str tag, CVec<Res> items) {
+        Res res = isStack ? b.stack() : b.line();
+        b.add_at(res, string("<" + tag + ">"_ss));
+        b.add_at(res, items);
+        b.add_at(res, string("</" + tag + ">"_ss));
+        return res;
+    }
+
+    Res stackWrap(Str const& tag, CVec<Res> items) {
+        return directionWrap(true, tag, items);
+    }
+
+    Res lineWrap(Str const& tag, CVec<Res> items) {
+        return directionWrap(false, tag, items);
+    }
+
+    void visitParagraph(Res& res, In<sem::Paragraph> tree) {
+        res = lineSubnodes(tree);
+        //        res = lineWrap("p", {lineSubnodes(tree)});
+    }
+
+
+    Res createTocList(sem::SemId<sem::Org> node);
+
+    void visitSubtree(Res& res, In<sem::Subtree> tree);
+
+    void visitStmtList(Res& res, In<sem::StmtList> doc) {
+        res = stackSubnodes(doc);
+    }
+
+    void visitVerbatim(Res& res, In<sem::Verbatim> verb) {
+        res = lineSubnodes(verb);
+    }
+
+    void visitMarkQuote(Res& res, In<sem::MarkQuote> mark) {
+        res = b.line({string("\""), lineSubnodes(mark), string("\"")});
+    }
+
+    void visitTimeRange(Res& res, In<sem::TimeRange> range) {
+        res = b.line({eval(range->from), string("--"), eval(range->to)});
+    }
+
+    void visitLink(Res& res, In<sem::Link> link) {
+        if (link->description) {
+            res = eval(link->description.value());
+        } else {
+            res = string("");
+        }
+    }
+
+    void visitDocument(Res& res, In<sem::Document> doc);
+
+#define __leaf(__Kind)                                                    \
+    void visit##__Kind(Res& res, In<sem::__Kind> word) {                  \
+        res = string(word->text);                                         \
+    }
+
+    EACH_SEM_ORG_LEAF_KIND(__leaf)
+
+#undef __leaf
+};
+
+extern template class Exporter<ExporterHtml, layout::BlockId>;
+
+#endif // EXPORTERHTML_HPP
--- src/editor/org_qml_node_edit.cpp
+++ src/editor/org_qml_node_edit.cpp
@@ -1,17 +1,81 @@
 #include "org_qml_node_edit.hpp"
-#include "exporter_qtextdocument.hpp"
+#include <lexbor/html/parser.h>
+#include <lexbor/html/interface.h>
+#include <lexbor/html/node.h>
+#include <lexbor/html/serialize.h>
+#include "exporterhtml.hpp"
 
-void OrgNodeTextWrapper::setRichText(const QString& value) {
-    if (cursor->node->is(OrgSemKind::Word)) {
-        cursor->node.getAs<sem::Word>()->text = to_std(value);
+using LxbDoc = SPtr<lxb_html_document_t>;
+using namespace sem;
+
+LxbDoc parseHtmlFragmentToLxb(const QString& html) {
+    QByteArray result   = html.toUtf8();
+    LxbDoc     document = LxbDoc{
+        lxb_html_document_create(),
+        lxb_html_document_destroy,
+    };
+
+    if (document == nullptr) { LOG(FATAL) << "Cannot create"; }
+
+    const lexbor_str_t lxb_html = lexbor_str(result.constData());
+    lxb_status_t       status   = lxb_html_document_parse(
+        document.get(), lxb_html.data, lxb_html.length);
+
+    if (status != LXB_STATUS_OK) { LOG(FATAL) << "Cannot parse"; }
+
+    return document;
+}
+
+void printHtmlStructure(lxb_dom_node_t* node, int depth = 0) {
+    if (node == nullptr) { return; }
+    std::cout << std::string(depth * 2, ' ');
+
+    switch (node->type) {
+        case LXB_DOM_NODE_TYPE_ELEMENT: {
+            auto           element  = lxb_dom_interface_element(node);
+            CP<lxb_char_t> tag_name = lxb_dom_element_qualified_name(
+                element, nullptr);
+
+            std::cout << "<element> "
+                      << std::string(
+                             reinterpret_cast<const char*>(tag_name));
+            break;
+        }
+        case LXB_DOM_NODE_TYPE_TEXT: {
+            auto           text = lxb_dom_interface_text(node);
+            std::size_t    len{};
+            CP<lxb_char_t> text_content = lxb_dom_node_text_content(
+                node, &len);
+            std::cout << "<text> "
+                      << std::string(
+                             reinterpret_cast<const char*>(text_content));
+        }
+        default: {
+            std::cout << fmt1(node->type);
+        }
+    }
+
+    std::cout << "\n";
+
+
+    // Recursively iterate over sub nodes
+    lxb_dom_node_t* sub = node->first_child;
+    while (sub != nullptr) {
+        printHtmlStructure(sub, depth + 1);
+        sub = sub->next;
     }
 }
 
+void OrgNodeTextWrapper::setRichText(const QString& value) {
+    LxbDoc doc = parseHtmlFragmentToLxb(value);
+    printHtmlStructure(lxb_dom_interface_node(doc.get()));
+}
+
 QString OrgNodeTextWrapper::getRichText() {
-    ExporterQTextDocument exp;
-    exp.addNewline = false;
-    exp.evalTop(cursor->node);
-    return exp.document->toHtml();
+    ExporterHtml    exp;
+    layout::BlockId block  = exp.evalTop(cursor->node);
+    Str             result = exp.store.toString(block);
+    return QString::fromStdString(result);
 }
 
 QVariant OrgNodeCursor::getNodeHandle() {
