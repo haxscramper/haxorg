fde105a10e820b6f538f53c061b497105f7f6743
haxscramper <haxscramper@gmail.com>
2024-04-18 23:36:10 +0400

    repo: move ~hstd~ library in cmake sub-project

--- src/cmake/target_lsp.cmake
+++ src/cmake/target_lsp.cmake
@@ -3,6 +3,6 @@ target_link_libraries(lsp PUBLIC haxorg)
 set_common_files(lsp)
 set_target_output(lsp)
 set_target_flags(lsp)
-glob_add_sources2(lsp "${BASE}/src/lsp/.*")
+glob_add_sources2(lsp "${BASE}/src/lsp/.*" "${BASE}")
 target_link_libraries(lsp PUBLIC haxorg absl::log absl::base)
 
