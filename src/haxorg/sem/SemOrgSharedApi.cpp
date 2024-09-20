a2bf0817ee8ba24dc9f1517b3a8cd69e41d29820
haxscramper <haxscramper@gmail.com>
2024-09-20 21:22:04 +0400

    org: shared API implementation comments

--- src/haxorg/sem/SemOrgSharedApi.cpp
+++ src/haxorg/sem/SemOrgSharedApi.cpp
@@ -77,13 +77,19 @@ template <sem::IsOrg T>
 Vec<sem::SemId<sem::Org>> getSubnodes(T const* t) { return t->subnodes; }
 // clang-format on
 
+/// \brief Cast sem ID type to target
 template <sem::IsOrg Out, sem::IsOrg In>
 sem::SemId<Out> org_cast(sem::SemId<In> arg) {
     return arg.template as<Out>();
 }
 
+/// \brief Cast imm ID type to target.
+///
+/// The target is specified as sem ID, but return type is mapped to imm ID.
+/// This is made for uniform API and more streamlined use on the callsite.
 template <sem::IsOrg Out, org::IsImmOrgValueType In>
-org::ImmAdapterT<Out> org_cast(org::ImmAdapterT<In> arg) {
+org::ImmAdapterT<typename org::sem_to_imm_map<Out>::imm_type> org_cast(
+    org::ImmAdapterT<In> arg) {
     return arg.template as<typename org::sem_to_imm_map<Out>::imm_type>();
 }
 
@@ -93,21 +99,26 @@ org::ImmAdapterT<typename org::sem_to_imm_map<Out>::imm_type> org_cast(
     return arg.template as<typename org::sem_to_imm_map<Out>::imm_type>();
 }
 
+/// brief Helper function to check if value is 'empty' -- used to
+/// streamline `Box<Opt<Imm>>` vs `Opt<Sem>` type handling.
 template <typename T>
 bool isBoolFalse(T const& t) {
     return value_metadata<T>::isEmpty(t);
 }
 
+/// \brief Convert immer ID to the imm adapter.
 template <org::IsImmOrgValueType T, typename Handle>
 org::ImmAdapterT<T> toHandle(org::ImmIdT<T> id, Handle const& handle) {
     return org::ImmAdapterT<T>{id, handle.ctx, {}};
 }
 
+/// \brief Convert sem ID to the sem ID -- identity function.
 template <sem::IsOrg T, typename Handle>
 sem::SemId<T> toHandle(sem::SemId<T> id, Handle const& handle) {
     return id;
 }
 
+/// \brief Convert optional imm ID to optional adapter
 template <org::IsImmOrgValueType T, typename Handle>
 Opt<org::ImmAdapterT<T>> toHandle(
     Opt<org::ImmIdT<T>> id,
@@ -119,6 +130,7 @@ Opt<org::ImmAdapterT<T>> toHandle(
     }
 }
 
+/// \brief Convert boxed optional imm ID to optional adapter
 template <org::IsImmOrgValueType T, typename Handle>
 Opt<org::ImmAdapterT<T>> toHandle(
     ImmBox<Opt<org::ImmIdT<T>>> id,
@@ -131,6 +143,8 @@ Opt<sem::SemId<T>> toHandle(Opt<sem::SemId<T>> id, Handle const& handle) {
     return id;
 }
 
+/// \brief Unwrap type `T` and get underlying sem/imm type, define nested
+/// `ast_type` for result.
 template <typename T>
 struct get_ast_type {};
 
@@ -148,15 +162,22 @@ template <typename T>
 struct get_ast_type<T*> {
     using ast_type = T;
 };
+
+/// \brief Select between sem org type and imm org type based on the
+/// leading typename `T`
+///
+/// If `T` is a sem type, define nested `result = SemType`, otherwise
+/// define `result = ImmType`.
 template <typename T, typename SemType, typename ImmType>
 struct SemOrImmType {};
 
-
+/// \brief Org type selector specialization to select sem type
 template <sem::IsOrg T, sem::IsOrg SemType, org::IsImmOrgValueType ImmType>
 struct SemOrImmType<T, SemType, ImmType> {
     using result = SemType;
 };
 
+/// \brief Org type selector specialization to select immer type
 template <
     org::IsImmOrgValueType T,
     sem::IsOrg             SemType,
@@ -165,17 +186,36 @@ struct SemOrImmType<T, SemType, ImmType> {
     using result = ImmType;
 };
 
+/// \brief Generic implementation of the subtree period collection --
+/// subtree period type is a shared data type, so in this function only
+/// parametrizing on the handle parameter.
 template <typename Handle>
 Vec<sem::SubtreePeriod> subtreeGetTimePeriodsImpl(
     Handle                           handle,
     IntSet<sem::SubtreePeriod::Kind> kinds) {
     Vec<sem::SubtreePeriod> res;
-    auto                    h = getConstHandle(handle);
-    using HandleBase          = get_ast_type<Handle>::ast_type;
+    // Get final handle version that can access fields down the line
+    auto h = getConstHandle(handle);
+    // `sem::T` or `sem::ImmT` type for later compile-time selection
+    using HandleBase = get_ast_type<Handle>::ast_type;
     using sem::SubtreePeriod;
+
+    // From now on, frequent usage of `toHandle` is needed to convert immer
+    // ID nodes to adapters that can be property accessed -- immer AST
+    // types do not store /full/ context necessary for accessing the final
+    // value, instead they only store ID elements. And `toHandle` is needed
+    // in order to take an ID and parent adapter (which has necessary
+    // context pointers), and return an new adapter.
+    //
+    // For sem IDs this is an identity function that returns the ID
+    // directly.
+
     for (const auto& it : getSubnodes(toHandle(h->title, handle))) {
         if (it->getKind() == OrgSemKind::Time) {
             SubtreePeriod period{};
+            // Specifying `sem::Time` in the cast, but returned type also
+            // depends on the type of the adapter -- if this is an immer
+            // adapter, the final cast would be to `org::ImmTime`,
             period.from = org_cast<sem::Time>(it)->getStatic().time;
             period.kind = SubtreePeriod::Kind::Titled;
             res.push_back(period);
@@ -227,6 +267,12 @@ Vec<sem::SubtreePeriod> subtreeGetTimePeriodsImpl(
     }
 
     if (kinds.contains(SubtreePeriod::Kind::Clocked)) {
+        // Subtree log is an AST type which has nested enum definitions,
+        // which are identical in structure, but from the compiler
+        // perspective `org::ImmSubtreeLog::Kind` and
+        // `sem::SubtreeLog::Kind` are two different classes. That's why I
+        // need to use the type selector here and define the target log
+        // type.
         using LogType = SemOrImmType<
             HandleBase,
             sem::SubtreeLog,
