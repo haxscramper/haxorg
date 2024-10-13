9c0391a29cf7ce6dc73d7344bdc3af914120d49d
haxscramper <haxscramper@gmail.com>
2024-10-13 21:23:30 +0400

    gui: write adaptagrams IR to JSON file

--- examples/imgui_gui/block_graph.cpp
+++ examples/imgui_gui/block_graph.cpp
@@ -25,6 +25,77 @@ struct JsonSerde<Slice<T>> {
     }
 };
 
+template <typename A, typename B>
+struct JsonSerde<Pair<A, B>> {
+    static Pair<A, B> from_json(json const& j) {
+        return std::make_pair<A, B>(
+            JsonSerde<A>::from_json(j["first"]),
+            JsonSerde<B>::from_json(j["second"]));
+    }
+
+    static json to_json(Pair<A, B> const& value) {
+        return json::object({
+            {"first", JsonSerde<A>::to_json(value.first)},
+            {"second", JsonSerde<B>::to_json(value.second)},
+        });
+    }
+};
+
+template <typename K, typename V>
+struct JsonSerde<UnorderedMap<K, V>> {
+    static json to_json(UnorderedMap<K, V> const& it) {
+        auto result = json::array();
+        for (auto const& [key, val] : it) {
+            result.push_back(json::object({
+                {"key", JsonSerde<K>::to_json(key)},
+                {"value", JsonSerde<V>::to_json(val)},
+            }));
+        }
+
+        return result;
+    }
+    static UnorderedMap<K, V> from_json(json const& j) {
+        UnorderedMap<K, V> result;
+        auto               tmp = result.transient();
+        for (auto const& i : j) {
+            result.insert(
+                JsonSerde<K>::from_json(i["key"]),
+                JsonSerde<V>::from_json(i["value"]));
+        }
+        return tmp.persistent();
+    }
+};
+
+template <IsVariant V>
+struct JsonSerde<V> {
+    static json to_json(V const& it) {
+        auto result     = json::object();
+        result["index"] = JsonSerde<int>::to_json(it.index());
+        std::visit(
+            [&]<typename T>(T const& value) {
+                result["value"] = JsonSerde<T>::to_json(value);
+            },
+            it);
+
+        return result;
+    }
+    static V from_json(json const& j) {
+        V result = variant_from_index<V>(j["index"].get<int>());
+        std::visit(
+            [&]<typename T>(T& value) {
+                value = JsonSerde<T>::from_json(j["value"]);
+            },
+            result);
+    }
+};
+
+template <>
+struct JsonSerde<double> {
+    static json   to_json(double const& it) { return json(it); }
+    static double from_json(json const& j) { return j.get<double>(); }
+};
+
+
 GraphNodeConstraint::Align::Spec spec(int rect) {
     return GraphNodeConstraint::Align::Spec{.node = rect};
 }
@@ -57,14 +128,17 @@ DocLayout to_layout(DocGraph const& g) {
                 DocNode next{.lane = lane_idx, .row = next_row};
                 lyt.ir.nodeConstraints.push_back(
                     GraphNodeConstraint{GraphNodeConstraint::Separate{
-                        .left  = {Vec{spec(lyt.rectMap.at(node))}},
-                        .right = {Vec{spec(lyt.rectMap.at(next))}},
+                        .left = GraphNodeConstraint::
+                            Align{.nodes = Vec{spec(lyt.rectMap.at(node))}, .dimension = GraphDimension::YDIM},
+                        .right = GraphNodeConstraint::
+                            Align{.nodes = Vec{spec(lyt.rectMap.at(next))}, .dimension = GraphDimension::YDIM},
                         .separationDistance //
                         = float(
                               lane.blocks.at(row).height
                               + lane.blocks.at(next_row).height + 100)
                         / 2.0f,
                         .isExactSeparation = true,
+                        .dimension         = GraphDimension::YDIM,
                     }});
             }
         }
@@ -97,6 +171,7 @@ DocLayout to_layout(DocGraph const& g) {
                     .separationDistance //
                     = float(lane_width + next_width + 100) / 2.0f,
                     .isExactSeparation = true,
+                    .dimension         = GraphDimension::XDIM,
                 }});
         }
     }
@@ -126,43 +201,42 @@ DocLayout to_layout(DocGraph const& g) {
 DocNode n(int lane, int row) { return DocNode{.lane = lane, .row = row}; }
 
 void run_block_graph_test() {
+    int  w     = 75;
+    int  h     = 25;
     auto lane0 = Vec<DocBlock>{
         // 0.0
-        DocBlock{.width = 100, .height = 200, .outEdges = {n(1, 0)}},
+        DocBlock{.width = w, .height = h, .outEdges = {n(1, 0)}},
         // 0.1
         DocBlock{
-            .width    = 100,
-            .height   = 250,
+            .width    = w,
+            .height   = h,
             .outEdges = {n(1, 1), n(1, 2), n(1, 3)}},
         // 0.2
-        DocBlock{.width = 100, .height = 280, .outEdges = {n(1, 4)}},
+        DocBlock{.width = w, .height = h, .outEdges = {n(1, 4)}},
     };
 
     auto lane1 = Vec<DocBlock>{
         // 1.0
-        DocBlock{.width = 100, .height = 200, .outEdges = {n(2, 0)}},
+        DocBlock{.width = w, .height = h, .outEdges = {n(2, 0)}},
         // 1.1
-        DocBlock{
-            .width = 100, .height = 250, .outEdges = {n(2, 0), n(2, 1)}},
+        DocBlock{.width = w, .height = h, .outEdges = {n(2, 0), n(2, 1)}},
         // 1.2
-        DocBlock{.width = 100, .height = 280, .outEdges = {n(2, 1)}},
+        DocBlock{.width = w, .height = h, .outEdges = {n(2, 1)}},
         // 1.3
-        DocBlock{
-            .width = 100, .height = 280, .outEdges = {n(2, 1), n(2, 2)}},
+        DocBlock{.width = w, .height = h, .outEdges = {n(2, 1), n(2, 2)}},
         // 1.4
-        DocBlock{
-            .width = 100, .height = 280, .outEdges = {n(2, 1), n(2, 3)}},
+        DocBlock{.width = w, .height = h, .outEdges = {n(2, 1), n(2, 3)}},
     };
 
     auto lane2 = Vec<DocBlock>{
         // 2.0
-        DocBlock{.width = 100, .height = 200},
+        DocBlock{.width = w, .height = h},
         // 2.1
-        DocBlock{.width = 100, .height = 250},
+        DocBlock{.width = w, .height = h},
         // 2.2
-        DocBlock{.width = 100, .height = 280},
+        DocBlock{.width = w, .height = h},
         // 2.3
-        DocBlock{.width = 100, .height = 280},
+        DocBlock{.width = w, .height = h},
     };
 
     DocGraph g{
@@ -181,8 +255,11 @@ void run_block_graph_test() {
             },
         }};
 
-    auto lyt = to_layout(g);
-    auto col = lyt.ir.doColaLayout();
+    auto lyt      = to_layout(g);
+    lyt.ir.height = 10000;
+    lyt.ir.width  = 10000;
+    auto col      = lyt.ir.doColaLayout();
     writeFile("/tmp/run_block_graph_test.json", to_json_eval(g).dump(2));
+    writeFile("/tmp/layout_ir.json", to_json_eval(lyt.ir).dump(2));
     col.writeSvg("/tmp/run_block_graph_test.svg");
 }
