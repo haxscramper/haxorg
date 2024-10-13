347e5e1ae221d6a713d02a396e1c51f39f1640c6
haxscramper <haxscramper@gmail.com>
2024-10-13 19:14:22 +0400

    gui: doc graph to adaptagrams layout wip

--- examples/imgui_gui/block_graph.cpp
+++ examples/imgui_gui/block_graph.cpp
@@ -1,4 +1,111 @@
 #include "block_graph.hpp"
 #include <hstd/wrappers/adaptagrams_wrap/adaptagrams_ir.hpp>
+#include <hstd/stdlib/Enumerate.hpp>
 
-void run_block_graph_test() {}
+struct DocLayout {
+    GraphLayoutIR              ir;
+    UnorderedMap<DocNode, int> rectMap;
+};
+
+DocLayout to_layout(DocGraph const& g) {
+    DocLayout lyt;
+
+    for (auto const& lane : enumerator(g.lanes)) {
+        for (auto const& row : lane.value().visibleRange) {
+            DocNode node{.lane = lane.index(), .row = row};
+            lyt.ir.rectangles.push_back(GraphSize{
+                .w = static_cast<double>(
+                    lane.value().blocks.at(row).width),
+                .h = static_cast<double>(
+                    lane.value().blocks.at(row).height),
+            });
+
+            int idx = lyt.ir.rectangles.high();
+            lyt.rectMap.insert_or_assign(node, idx);
+        }
+    }
+
+    for (auto const& lane : enumerator(g.lanes)) {
+        for (auto const& row : lane.value().visibleRange) {
+            DocNode source{.lane = lane.index(), .row = row};
+            for (auto const& target : g.at(source).outEdges) {
+                GraphEdge edge{
+                    lyt.rectMap.at(source),
+                    lyt.rectMap.at(target),
+                };
+                lyt.ir.edges.push_back(edge);
+                lyt.ir.edgeConstraints.insert_or_assign(
+                    edge,
+                    GraphEdgeConstraint{
+                        .sourcePort = GraphEdgeConstraint::Port::East,
+                        .targetPort = GraphEdgeConstraint::Port::West,
+                    });
+            }
+        }
+    }
+
+    return lyt;
+}
+
+DocNode n(int lane, int row) { return DocNode{.lane = lane, .row = row}; }
+
+void run_block_graph_test() {
+    auto lane0 = Vec<DocBlock>{
+        // 0.0
+        DocBlock{.width = 100, .height = 200, .outEdges = {n(1, 0)}},
+        // 0.1
+        DocBlock{
+            .width    = 100,
+            .height   = 250,
+            .outEdges = {n(1, 1), n(1, 2), n(1, 3)}},
+        // 0.2
+        DocBlock{.width = 100, .height = 280, .outEdges = {n(1, 4)}},
+    };
+
+    auto lane1 = Vec<DocBlock>{
+        // 1.0
+        DocBlock{.width = 100, .height = 200, .outEdges = {n(2, 0)}},
+        // 1.1
+        DocBlock{
+            .width = 100, .height = 250, .outEdges = {n(2, 0), n(2, 1)}},
+        // 1.2
+        DocBlock{.width = 100, .height = 280, .outEdges = {n(2, 1)}},
+        // 1.3
+        DocBlock{
+            .width = 100, .height = 280, .outEdges = {n(2, 1), n(2, 2)}},
+        // 1.4
+        DocBlock{
+            .width = 100, .height = 280, .outEdges = {n(2, 1), n(2, 3)}},
+    };
+
+    auto lane2 = Vec<DocBlock>{
+        // 2.0
+        DocBlock{.width = 100, .height = 200},
+        // 2.1
+        DocBlock{.width = 100, .height = 250},
+        // 2.2
+        DocBlock{.width = 100, .height = 280},
+        // 2.3
+        DocBlock{.width = 100, .height = 280},
+    };
+
+    DocGraph g{
+        .lanes = {
+            DocBlockStack{
+                .blocks       = lane0,
+                .visibleRange = slice(0, 2),
+            },
+            DocBlockStack{
+                .blocks       = lane1,
+                .visibleRange = slice(0, 4),
+            },
+            DocBlockStack{
+                .blocks       = lane2,
+                .visibleRange = slice(0, 3),
+            },
+        }};
+
+    auto lyt = to_layout(g);
+    auto col = lyt.ir.doColaLayout();
+    col.writeSvg("/tmp/run_block_graph_test.svg");
+}
--- examples/imgui_gui/block_graph.hpp
+++ examples/imgui_gui/block_graph.hpp
@@ -2,20 +2,46 @@
 
 #include <hstd/stdlib/Vec.hpp>
 
-struct DocBlock {
-    int      width;
-    int      height;
-    Vec<int> outPortPositions;
-    Vec<int> inPortPositions;
+struct DocNode {
+    int  lane;
+    int  row;
+    bool operator==(DocNode const& other) const {
+        return lane == other.lane && row == other.row;
+    }
+    DESC_FIELDS(DocNode, (lane, row));
+};
 
-    DESC_FIELDS(
-        DocBlock,
-        (width, height, outPortPositions, inPortPositions));
+struct DocBlock {
+    int          width;
+    int          height;
+    Vec<DocNode> outEdges;
+
+    DESC_FIELDS(DocBlock, (width, height, outEdges));
 };
 
 struct DocBlockStack {
     Vec<DocBlock> blocks;
     Slice<int>    visibleRange;
+    DESC_FIELDS(DocBlockStack, (blocks, visibleRange));
+};
+
+template <>
+struct std::hash<DocNode> {
+    std::size_t operator()(DocNode const& it) const noexcept {
+        std::size_t result = 0;
+        hax_hash_combine(result, it.lane);
+        hax_hash_combine(result, it.row);
+        return result;
+    }
+};
+
+
+struct DocGraph {
+    Vec<DocBlockStack> lanes;
+
+    DocBlock const& at(DocNode const& node) const {
+        return lanes.at(node.lane).blocks.at(node.row);
+    }
 };
 
 void run_block_graph_test();
