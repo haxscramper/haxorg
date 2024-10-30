17822cca479f8b26192b37db8e3d66ba846e1f7f
haxscramper <haxscramper@gmail.com>
2024-10-30 21:20:20 +0400

    std: more tests for the DOD IDs

--- tests/hstd/tDOD.cpp
+++ tests/hstd/tDOD.cpp
@@ -3,7 +3,11 @@
 #include <hstd/stdlib/dod_base.hpp>
 #include <bitset>
 
-DECL_ID_TYPE_MASKED(Value1, Id1, u8, 2);
+struct IdMask2Value {};
+DECL_ID_TYPE_MASKED(IdMask2Value, IdMask2, u8, 2);
+
+struct IdMask0Value {};
+DECL_ID_TYPE_MASKED(IdMask0Value, IdMask0, u8, 0);
 
 template <typename T>
 std::string bits(T val) {
@@ -12,18 +16,39 @@ std::string bits(T val) {
 
 
 TEST(DODContainersTest, MaskedNodes) {
-    Id1 id{0};
+    IdMask2 id = IdMask2::FromValue(0);
+    EXPECT_EQ(bits(id.getValue()), std::string("00000000"));
     id.setValue(1_u8);
+    EXPECT_EQ(bits(id.getValue()), std::string("00000001"));
     EXPECT_EQ(id.getMaskSize(), 2);
     EXPECT_EQ(id.getValue(), 1_u8);
     EXPECT_EQ(id.getValue(), 0b1_u8);
     id.setMask(2_u8);
-    EXPECT_EQ(bits(id.getValue()), std::string("01000001"));
+    EXPECT_EQ(bits(id.getValue()), std::string("10000001"));
     EXPECT_EQ(id.getMask(), 2_u8);
+    EXPECT_EQ(bits(id.getMask()), std::string("00000010"));
+    EXPECT_EQ(bits(id.getMaskUnshifed()), std::string("10000000"));
     EXPECT_EQ(id.getIndex(), 0);
     {
-        u8 m = (1_u8) | (2_u8 << Id1::mask_offset);
+        u8 m = (1_u8) | (2_u8 << IdMask2::mask_offset);
         EXPECT_EQ(id.getValue(), m);
     }
     EXPECT_EQ(id.getUnmasked(), 1);
+
+    id.setMask(3_u8);
+    EXPECT_EQ(bits(id.getValue()), std::string("11000001"));
+    EXPECT_EQ(id.getMask(), 3_u8);
+    EXPECT_EQ(id.getIndex(), 0);
+}
+
+TEST(DODContainersTest, UnmaskedNodes) {
+    IdMask0 id = IdMask0::FromValue(0);
+    EXPECT_EQ(bits(id.getValue()), std::string("00000000"));
+    EXPECT_TRUE(id.isNil());
+    id.setValue(1_u8);
+    EXPECT_EQ(bits(id.getValue()), std::string("00000001"));
+    EXPECT_FALSE(id.isNil());
+    EXPECT_EQ(id.getMask(), 0);
+    EXPECT_EQ(id.getMaskSize(), 0);
+    EXPECT_EQ(id.getMaskUnshifed(), 0);
 }
