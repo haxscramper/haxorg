0c5b9b5992a0d85ff8291e887540072f9c8786d2
haxscramper <haxscramper@gmail.com>
2025-01-07 21:20:02 +0400

    web: add reflection system to auto-register funcs

--- examples/rest_access/main.cpp
+++ examples/rest_access/main.cpp
@@ -11,6 +11,9 @@
 #include <haxorg/sem/SemBaseApi.hpp>
 #include <haxorg/exporters/ExporterJson.hpp>
 #include <haxorg/sem/ImmOrg.hpp>
+#include <functional>
+#include <tuple>
+#include <type_traits>
 
 #define OLOG(lvl) BOOST_LOG_TRIVIAL(lvl)
 
@@ -44,6 +47,242 @@ void read_opt_json_param_impl(
 }
 
 
+// Helper to get function traits
+template <typename T>
+struct function_traits;
+
+// Function pointer specialization
+template <typename R, typename... Args>
+struct function_traits<R (*)(Args...)> {
+    using return_type             = R;
+    using args_tuple              = std::tuple<std::decay_t<Args>...>;
+    static constexpr size_t arity = sizeof...(Args);
+};
+
+// Method pointer specialization
+template <typename C, typename R, typename... Args>
+struct function_traits<R (C::*)(Args...) const> {
+    using class_type              = C;
+    using return_type             = R;
+    using args_tuple              = std::tuple<std::decay_t<Args>...>;
+    static constexpr size_t arity = sizeof...(Args);
+};
+
+struct ArgSpec {
+    std::string name;
+    bool        optional = false;
+};
+
+ArgSpec arg(std::string const& name) { return ArgSpec{.name = name}; }
+ArgSpec opt_arg(std::string const& name) {
+    return ArgSpec{.name = name, .optional = true};
+}
+
+void standalone_function(int arg1, int arg2, int opt1 = 123) {
+    OLOG(info) << fmt(
+        "Called standalone function {} {} {}", arg1, arg2, opt1);
+}
+
+struct RestHandlerContext {
+    UnorderedMap<std::string, std::string>             query_params;
+    std::shared_ptr<http::response<http::string_body>> response;
+    std::string                                        route;
+    json                                               query_body;
+
+    RestHandlerContext(
+        std::shared_ptr<http::response<http::string_body>> response)
+        : response{response} {}
+
+    template <typename T>
+    T getArg(ArgSpec const& arg) {
+        return query_body.at(arg.name).get<T>();
+    }
+
+    template <typename T>
+    T getThis() {
+        return getArg<T>(ArgSpec{"self"});
+    }
+
+    void setResponseBody(std::string const& text) {
+        response->body() = text;
+    }
+
+    void setResponseResult(boost::beast::http::status status) {
+        response->result(status);
+    }
+
+
+    void setRequest(http::request<http::string_body> const& req) {
+        setTarget(req.target());
+        setQueryBody(req.body());
+    }
+
+    void setQueryBody(std::string_view body) {
+        query_body = json::parse(body);
+    }
+
+    void setTarget(std::string_view target) {
+        if (auto pos = target.find('?'); pos != std::string_view::npos) {
+            std::string_view query = target.substr(pos + 1);
+            std::string_view route = target.substr(0, pos);
+
+            std::string        param;
+            std::istringstream query_stream{std::string(query)};
+            while (std::getline(query_stream, param, '&')) {
+                if (auto eq_pos = param.find('=');
+                    eq_pos != std::string::npos) {
+                    query_params.insert_or_assign(
+                        param.substr(0, eq_pos), param.substr(eq_pos + 1));
+                }
+            }
+            this->route = route;
+        } else {
+            this->route = target;
+        }
+    }
+};
+
+// Handler implementation
+class HandlerImpl {
+    std::function<void(RestHandlerContext*)> callback_;
+    std::vector<ArgSpec>                     args_;
+    size_t                                   required_args_;
+
+  public:
+    template <typename F>
+    HandlerImpl(F&& f, size_t req_args, std::vector<ArgSpec> args)
+        : callback_(std::forward<F>(f))
+        , args_(std::move(args))
+        , required_args_(req_args) {}
+
+    void call(RestHandlerContext* ctx) { callback_(ctx); }
+};
+
+// Helper for unpacking tuple into function arguments
+template <typename Func, typename Tuple, size_t... I>
+auto call_with_tuple_impl(Func&& f, Tuple&& t, std::index_sequence<I...>) {
+    return std::forward<Func>(f)(std::get<I>(std::forward<Tuple>(t))...);
+}
+
+template <typename Func, typename Tuple>
+auto call_with_tuple(Func&& f, Tuple&& t) {
+    return call_with_tuple_impl(
+        std::forward<Func>(f),
+        std::forward<Tuple>(t),
+        std::make_index_sequence<
+            std::tuple_size_v<std::remove_reference_t<Tuple>>>{});
+}
+
+// Handler construction helpers
+class HandlerMapType {
+    UnorderedMap<std::string, HandlerImpl> map;
+
+    template <typename Func, typename... Args>
+    auto make_handler_callback(
+        Func                        f,
+        std::vector<ArgSpec> const& arg_specs) {
+        return [f, arg_specs](RestHandlerContext* ctx) {
+            using traits      = function_traits<std::decay_t<Func>>;
+            using args_tuple  = typename traits::args_tuple;
+            using return_type = typename traits::return_type;
+
+            // Build tuple of arguments
+            args_tuple args;
+            std::apply(
+                [&](auto&... tuple_args) {
+                    size_t idx = 0;
+                    ((tuple_args = ctx->template getArg<
+                                   std::decay_t<decltype(tuple_args)>>(
+                          arg_specs[idx++])),
+                     ...);
+                },
+                args);
+
+            // Call function and serialize result
+            if constexpr (std::is_void_v<return_type>) {
+                call_with_tuple(f, args);
+            } else {
+                auto result = call_with_tuple(f, args);
+                ctx->setResponseBody(
+                    JsonSerde<return_type>::to_json(result).dump());
+            }
+            ctx->setResponseResult(http::status::ok);
+        };
+    }
+
+    template <typename Class, typename Func>
+    auto make_method_handler_callback(
+        Func                        f,
+        std::vector<ArgSpec> const& arg_specs) {
+        return [f, arg_specs](RestHandlerContext* ctx) {
+            using traits      = function_traits<std::decay_t<Func>>;
+            using args_tuple  = typename traits::args_tuple;
+            using return_type = typename traits::return_type;
+
+            // Get instance
+            auto instance = ctx->template getThis<Class>();
+
+            // Build tuple of arguments
+            args_tuple args;
+            std::apply(
+                [&](auto&... tuple_args) {
+                    size_t idx = 0;
+                    ((tuple_args = ctx->template getArg<
+                                   std::decay_t<decltype(tuple_args)>>(
+                          arg_specs[idx++])),
+                     ...);
+                },
+                args);
+
+            // Call method and serialize result
+            auto result = call_with_tuple(
+                [&instance, f](auto&&... args) {
+                    return (instance.*f)(
+                        std::forward<decltype(args)>(args)...);
+                },
+                args);
+
+            if constexpr (!std::is_void_v<return_type>) {
+                ctx->setResponseBody(
+                    JsonSerde<return_type>::to_json(result).dump());
+            }
+            ctx->setResponseResult(http::status::ok);
+        };
+    }
+
+  public:
+    template <typename Func>
+    void setFunction(
+        std::string const&          endpoint,
+        Func                        f,
+        int                         required_args,
+        std::vector<ArgSpec> const& args) {
+        map.emplace(
+            endpoint,
+            HandlerImpl(
+                make_handler_callback(f, args), required_args, args));
+    }
+
+    template <typename Class, typename Func>
+    void setMethod(
+        std::string const&          endpoint,
+        Func                        f,
+        int                         required_args,
+        std::vector<ArgSpec> const& args) {
+        map.emplace(
+            endpoint,
+            HandlerImpl(
+                make_method_handler_callback<Class>(f, args),
+                required_args,
+                args));
+    }
+
+    void call(std::string const& target, RestHandlerContext* ctx) {
+        map.at(target).call(ctx);
+    }
+};
+
+
 #define opt_query_param(map, obj, field)                                  \
     read_opt_query_param_impl(map, obj, &decltype(obj)::field, #field);
 
@@ -53,6 +292,7 @@ void read_opt_json_param_impl(
 struct HttpState : public SharedPtrApi<HttpState> {
     org::ImmAstContext::Ptr ctx;
     org::ImmAstVersion      root;
+    HandlerMapType          handlers;
 
     void parseRoot(std::string const& text) {
         root = ctx->addRoot(sem::parseString(text));
@@ -69,8 +309,8 @@ class HttpSession : public SharedPtrApi<HttpSession> {
     HttpState::Ptr                   state;
 
   public:
-    explicit HttpSession(tcp::socket&& socket)
-        : stream(std::move(socket)) {
+    explicit HttpSession(tcp::socket&& socket, HttpState::Ptr state)
+        : stream(std::move(socket)), state{state} {
         OLOG(info) << "Created HTTP session";
     }
 
@@ -93,6 +333,7 @@ class HttpSession : public SharedPtrApi<HttpSession> {
     }
 
     void process_request() {
+        LOGIC_ASSERTION_CHECK(state.get() != nullptr, "state is not set");
         auto response = std::make_shared<
             http::response<http::string_body>>();
         response->version(req.version());
@@ -100,51 +341,30 @@ class HttpSession : public SharedPtrApi<HttpSession> {
         response->set(http::field::content_type, "application/json");
 
         // Get the request target/route
-        std::string_view target = req.target();
+        RestHandlerContext ctx{response};
+        ctx.setRequest(req);
 
-        OLOG(info) << fmt("Received request:'{}'", target);
-        OLOG(info) << fmt("Body:\n{}", req.body());
-
-        json query_body = json::parse(req.body());
-
-        // Parse query parameters if present
-        UnorderedMap<std::string, std::string> query_params;
-        if (auto pos = target.find('?'); pos != std::string_view::npos) {
-            std::string_view query = target.substr(pos + 1);
-            std::string_view route = target.substr(0, pos);
-
-            std::string        param;
-            std::istringstream query_stream{std::string(query)};
-            while (std::getline(query_stream, param, '&')) {
-                if (auto eq_pos = param.find('=');
-                    eq_pos != std::string::npos) {
-                    query_params.insert_or_assign(
-                        param.substr(0, eq_pos), param.substr(eq_pos + 1));
-                }
-            }
-            target = route;
-        }
-
-        OLOG(info) << fmt("Query parametersn {}", query_params);
+        OLOG(info) << fmt("Query parametersn {}", ctx.query_params);
 
         switch (req.method()) {
             case http::verb::post: {
-                if (target == "/api/parseString") {
+                if (ctx.route == "/api/parseString") {
                     response->result(http::status::ok);
                     ExporterJson exp{};
-                    opt_query_param(query_params, exp, skipEmptyLists);
-                    opt_query_param(query_params, exp, skipLocation);
-                    opt_query_param(query_params, exp, skipId);
-                    opt_query_param(query_params, exp, skipNullFields);
+                    opt_query_param(ctx.query_params, exp, skipEmptyLists);
+                    opt_query_param(ctx.query_params, exp, skipLocation);
+                    opt_query_param(ctx.query_params, exp, skipId);
+                    opt_query_param(ctx.query_params, exp, skipNullFields);
                     response->body() = exp
                                            .evalTop(sem::parseString(
-                                               query_body.at("text")
-                                                   .get<std::string>()))
+                                               ctx.getArg<std::string>(
+                                                   {"text"})))
                                            .dump();
-                } else if (target == "/api/parseRoot") {
-                    state->parseRoot(
-                        query_body.at("text").get<std::string>());
+                } else if (ctx.route == "/api/parseRoot") {
+                    state->parseRoot(ctx.getArg<std::string>({"text"}));
                     response->result(http::status::ok);
+                } else {
+                    state->handlers.call(ctx.route, &ctx);
                 }
                 break;
             }
@@ -204,7 +424,8 @@ class HttpServer : public SharedPtrApi<HttpServer> {
 
     void on_accept(beast::error_code ec, tcp::socket socket) {
         if (!ec) {
-            std::make_shared<HttpSession>(std::move(socket))->start();
+            std::make_shared<HttpSession>(std::move(socket), state)
+                ->start();
         }
         accept();
     }
@@ -212,7 +433,14 @@ class HttpServer : public SharedPtrApi<HttpServer> {
 
 int main() {
     try {
-        HttpState::Ptr  state   = std::make_shared<HttpState>();
+        HttpState::Ptr state = std::make_shared<HttpState>();
+
+        state->handlers.setFunction(
+            "/api/standalone",
+            &standalone_function,
+            2,
+            {arg("arg1"), arg("arg2"), opt_arg("opt1")});
+
         auto const      port    = 8080;
         auto const      threads = std::thread::hardware_concurrency();
         net::io_context ioc{static_cast<int>(threads)};
