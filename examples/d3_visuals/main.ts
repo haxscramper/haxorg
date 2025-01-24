5b548e0e2549a57553779a0eb1bad95c1a4debb1
haxscramper <haxscramper@gmail.com>
2025-01-24 20:19:38 +0400

    gui: format d3.js example code

--- examples/d3_visuals/main.ts
+++ examples/d3_visuals/main.ts
@@ -1,26 +1,32 @@
-import {app, BrowserWindow} from 'electron';
-import * as path from 'path';
+import {app, BrowserWindow} from "electron";
+import * as path from "path";
 
 async function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 1600,
-    height: 1200,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      webSecurity: false,                // need this to load local files
-      allowRunningInsecureContent: true  // In development
+    width : 1600,
+    height : 1200,
+    webPreferences : {
+      nodeIntegration : true,
+      contextIsolation : false,
+      webSecurity : false,               // need this to load local files
+      allowRunningInsecureContent : true // In development
     }
   });
 
-  await mainWindow.loadFile('renderer/index.html');
+  await mainWindow.loadFile("renderer/index.html");
   mainWindow.webContents.openDevTools();
+
+  process.on("SIGUSR1", () => {
+    if (mainWindow) {
+      mainWindow.reload();
+    }
+  });
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
