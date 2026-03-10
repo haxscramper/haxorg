74386035533c9dd51561dfc5ce859f7d7dcc5a57
haxscramper <haxscramper@gmail.com>
2026-03-10 19:04:41 +0400

    test(codegen): use cast in tests
    Add cast call for test typing

--- tests/python/refl/test_wrapper_generation.py
+++ tests/python/refl/test_wrapper_generation.py
@@ -2,10 +2,12 @@ import pytest
 
 from tests.python.conftest import stable_test_dir
 from py_codegen import astbuilder_nim, astbuilder_embind, astbuilder_py, astbuilder_nanobind
+from pathlib import Path
+from beartype.typing import cast
 
 
 @pytest.mark.test_release
-def test_structure_wrapping(stable_test_dir):
+def test_structure_wrapping(stable_test_dir: Path):
     from tests.python.refl import refl_test_driver
 
     wraps = refl_test_driver.get_all_code(
@@ -13,34 +15,39 @@ def test_structure_wrapping(stable_test_dir):
         stable_test_dir=stable_test_dir,
     )
 
-    struct_nim = wraps.getNimEntries("Defined")[0]
+    struct_nim = cast(astbuilder_nim.ObjectParams, wraps.getNimEntries("Defined")[0])
 
     assert struct_nim
     assert struct_nim.Name == "Defined"
 
-    struct_py = wraps.getPythonEntries("Defined")[0]
+    struct_py = cast(astbuilder_nanobind.NbClass, wraps.getPythonEntries("Defined")[0])
 
     assert struct_py
     assert struct_py.getPyName() == "Defined"
 
-    struct_em = wraps.getWasmEntries("Defined")[0]
+    struct_em = cast(astbuilder_embind.WasmClass, wraps.getWasmEntries("Defined")[0])
 
     assert struct_em
     assert struct_em.getWasmName() == "Defined"
 
 
 @pytest.mark.test_release
-def test_function_wrapping(stable_test_dir):
+def test_function_wrapping(stable_test_dir: Path):
     from tests.python.refl import refl_test_driver
     wraps = refl_test_driver.get_all_code("[[refl]] int defined();",
                                           stable_test_dir=stable_test_dir)
 
-    fun_nim = wraps.getNimEntries("defined")[0]
+    fun_nim = cast(astbuilder_nim.FunctionParams, wraps.getNimEntries("defined")[0])
 
     assert fun_nim
     assert fun_nim.Name == "defined"
 
-    fun_py: astbuilder_nanobind.NbFunction = wraps.getPythonEntries("defined")[0]
+    fun_py = cast(astbuilder_nanobind.NbFunction, wraps.getPythonEntries("defined")[0])
 
     assert fun_py
     assert fun_py.PyName == "defined"
+
+    fun_em = cast(astbuilder_embind.WasmFunction, wraps.getWasmEntries("defined")[0])
+
+    assert fun_em
+    assert fun_em.getWasmName() == "defined"
