a331d02b40713e246837713c1725cc23f9df0a88
haxscramper <haxscramper@gmail.com>
2026-04-07 08:56:02 +0400

    test(codegen): finalize first test for C wrappers
    Use newly introduced API for getting value stored in the optional, and
    check the node kind in the wrapped API.

--- tests/org/tOrgCApi.cpp
+++ tests/org/tOrgCApi.cpp
@@ -22,14 +22,28 @@ TEST(OrgCApi, TestSimpleParsing) {
 
     haxorg_StdString source_name = haxorg_create_StdString_ConstChar(
         &ctx, "<test>");
+    ASSERT_FALSE(ctx.has_error);
 
+    // parse, get Document node at root level
     haxorg_SemIdOfOrg parsed_node = haxorg_ParseContext_parseString(
         &ctx, parse_context, input_string, source_name);
+    ASSERT_FALSE(ctx.has_error);
 
+    // Access element at index 0, the text should be paragraph
     haxorg_StdOptional first_item = haxorg_SemIdOfOrg_getIndex(
         &ctx, parsed_node, 0);
+    ASSERT_FALSE(ctx.has_error);
 
-    // haxorg_ptr_payload first_item_node = haxorg_Std
+    // Unpack optional value for the paragraph
+    haxorg_ptr_payload first_item_node = haxorg_StdOptional_value(
+        &ctx, first_item);
+    ASSERT_FALSE(ctx.has_error);
+    haxorg_SemIdOfOrg stmt_node{.data = first_item_node};
+
+    haxorg_OrgSemKind node_kind = haxorg_SemIdOfOrg_getNodeKind_const(
+        &ctx, stmt_node);
+
+    EXPECT_EQ(node_kind, haxorg_OrgSemKind::Paragraph);
 
     haxorg_destroy_SemIdOfOrg(&ctx, &parsed_node);
 }
