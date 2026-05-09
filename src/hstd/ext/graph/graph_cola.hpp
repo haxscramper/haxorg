99d15a755e5f190e9b7542c3a09c5b5e3f52f365
haxscramper <haxscramper@gmail.com>
2026-05-09 19:40:02 +0400

    refactor(mmap): split avoid edge router to file
    Extract libavoid edge router logic into its own file.

--- src/hstd/ext/graph/graph_cola.hpp
+++ src/hstd/ext/graph/graph_cola.hpp
@@ -5,8 +5,8 @@
 #include <libdialect/hola.h>
 #include <hstd/ext/bimap_wrap.hpp>
 #include <hstd/stdlib/algorithms.hpp>
-#include <hstd/ext/logger.hpp>
 #include <hstd/ext/graph/graph_vpsc.hpp>
+#include <hstd/ext/graph/graph_avoid.hpp>
 
 namespace hstd::ext::graph {
 
@@ -231,12 +231,6 @@ class ColaVertexLayoutAttribute : public layout::IVertexLayoutAttribute {
     }
 };
 
-class ColaEdgeLayoutAttribute : public layout::IEdgeLayoutAttribute {
-  public:
-    geometry::Path path;
-    ColaEdgeLayoutAttribute(geometry::Path const& path) : path{path} {}
-    Path getPath() const override { return path; }
-};
 
 class ColaGroupLayoutAttribute : public layout::IGroupLayoutAttribute {
   public:
@@ -617,108 +611,6 @@ class PageBoundaryConstraint : public ColaConstraint {
         const override;
 };
 
-class AvoidPort : public IPort {
-  public:
-    std::size_t getHash() const override { return 0; }
-
-    bool isEqual(IGraphObjectBase const* other) const override {
-        return true;
-    }
-
-    std::string getRepr() const override { return ""; }
-
-    hstd::Vec<hstd::SPtr<IAttribute>> attrs;
-
-    hstd::Vec<hstd::SPtr<IAttribute>> getAttributes() const override {
-        return attrs;
-    }
-
-    void addAttribute(hstd::SPtr<IAttribute> const& attr) override {
-        attrs.push_back(attr);
-    }
-
-    void setAttributes(
-        hstd::Vec<hstd::SPtr<IAttribute>> const& attrs) override {
-        this->attrs = attrs;
-    }
-};
-
-class AvoidPortVisualAttribute : public layout::IPortVisualAttribute {
-  public:
-    DECL_DESCRIBED_ENUM(VisibilityDirection, Left, Right, Top, Bottom);
-    VisibilityDirection visibility;
-    hstd::Opt<double>   edgeOffset;
-};
-
-class AvoidPortLayoutAttribute : public layout::IPortLayoutAttribute {
-  public:
-    Rect getBBox() const override {
-        return Rect::FromCenterWH(Point(xOffset, yOffset), width, height);
-    }
-
-    visual::VisGroup getVisual(PortID const& id) const override {
-        visual::VisGroup res;
-        res.elements.push_back(
-            visual::VisElement{visual::VisElement::RectShape{getBBox()}});
-        return res;
-    }
-
-    DECL_DESCRIBED_ENUM(Placement, Unspecified, Proportional, Absolute);
-    using VisibilityDirection = AvoidPortVisualAttribute::
-        VisibilityDirection;
-    VisibilityDirection visibility;
-    Placement           placement;
-    double              xOffset;
-    double              yOffset;
-    double              width  = 1;
-    double              height = 1;
-
-    Avoid::ConnEnd             connection;
-    Avoid::ShapeConnectionPin* pin;
-};
-
-
-class AvoidPortCollection : public IPortCollection {
-    hstd::UnorderedIncrementalStore<PortID, AvoidPort> portStore;
-
-  public:
-    PortCollectionID getCategory() const override {
-        return hstd::ext::graph::PortCollectionID(
-            hstd::hash_bits<15>(typeid(this).hash_code()));
-    }
-
-    const IPort* getPort(PortID pid) const override {
-        return &portStore.at(pid);
-    }
-
-    PortID addPort(VertexID vertex, EdgeID edge, bool is_start) {
-        auto id = portStore.add(AvoidPort{});
-        IPortCollection::addPort(vertex, edge, is_start, id);
-        return id;
-    }
-};
-
-class AvoidRouterAlgorithm {
-  public:
-    /// \brief put a fake buffer around each shape so the edges would not
-    /// run perfectly flush against the nodes.
-    hstd::Opt<int>                       shapeBufferDistance = 1;
-    VpscRectTracker*                     rects;
-    layout::IGroupVisualAttribute const* group;
-    VertexID                             root_group = VertexID::Nil();
-    hstd::SPtr<layout::LayoutRun>        run;
-    layout::IPlacementAlgorithm::Result* intermediate_placement;
-
-    hstd::UnorderedMap<VertexID, Avoid::ShapeRef*> shapes;
-    hstd::UnorderedMap<EdgeID, Avoid::ConnRef*>    connections;
-    hstd::SPtr<Avoid::Router>                      router;
-
-    struct Result {
-        hstd::SPtr<AvoidPortCollection> layoutPorts;
-    };
-
-    Result routeEdges();
-};
 
 class ColaLayoutAlgorithm : public layout::IPlacementAlgorithm {
   public:
